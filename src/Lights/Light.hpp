086b49c501859ad7f755b9048bf7f662b48b47f0
Jaafer Sheriff <jaafersheriff@gmail.com>
2017-09-07 21:43:33 -0700

    Basic point light

---
+++ src/Lights/Light.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef _LIGHT_HPP_
+#define _LIGHT_HPP_
+
+#include "glm/glm.hpp"
+
+class Light {
+   public:
+      glm::vec3 position = glm::vec3(0, 0, 0);
+      glm::vec3 color = glm::vec3(1, 0, 0);
+      glm::vec3 attenuation = glm::vec3(1, 0, 0);
+
+
+      Light(glm::vec3 pos) {
+         position = pos;
+      }
+
+      Light(glm::vec3 pos, glm::vec3 col) {
+         position = pos;
+         color = col;
+      }
+
+};
+
+#endif
