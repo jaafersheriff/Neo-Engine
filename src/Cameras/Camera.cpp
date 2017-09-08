fa6e91c8330c9a2f948d8fde34d780afd74214dc
Jaafer Sheriff <jaafersheriff@gmail.com>
2017-09-07 21:43:16 -0700

    Floating camera

---
+++ src/Cameras/Camera.cpp
@@ -0,0 +1,57 @@
+#include "Camera.hpp"
+
+Camera::Camera(glm::vec3 position) {
+   this->position = position;
+   update(0.f, 0.f);
+}
+
+Camera::Camera() {
+   Camera(glm::vec3(0, 0, 0));
+}
+
+// dx and dy correspond to mouse movement 
+void Camera::update(float dx, float dy) {
+   // Update look at point
+   theta += dx * moveSpeed;
+   phi += dy * moveSpeed;
+   glm::vec3 sphere = glm::vec3(
+                  glm::cos(phi)*glm::cos(theta), 
+                  glm::sin(phi), 
+                  glm::cos(phi)*glm::cos(PI/2.f)-theta);
+   lookAt = position + glm::normalize(sphere);
+
+   // Update UVW
+   w = glm::normalize(lookAt - position);
+   u = glm::normalize(glm::cross(w, glm::vec3(0, 1, 0)));
+   v = glm::normalize(glm::cross(u, w));
+}
+
+void Camera::moveForward() { 
+   position += w;
+   lookAt += w;
+}
+
+void Camera::moveBackward() { 
+   position -= w;
+   lookAt -= w;
+}
+
+void Camera::moveRight() { 
+   position += u;
+   lookAt += u;
+}
+
+void Camera::moveLeft() { 
+   position -= u;
+   lookAt -= u;
+}
+
+void Camera::moveUp() { 
+   position += v;
+   lookAt += v;
+}
+
+void Camera::moveDown() { 
+   position -= v;
+   lookAt -= v;
+}
---
+++ src/Cameras/Camera.hpp
@@ -0,0 +1,33 @@
+// Parent Camera class
+// This Camera can be used as a floating camera with no bounds
+// Subclasses of this Camera can impose limiations/locks on this Camera  
+#pragma once
+#ifndef _CAMERA_HPP_
+#define _CAMERA_HPP_
+
+#include "glm/glm.hpp"
+#include "Toolbox/Toolbox.hpp"
+
+class Camera {
+   public:
+      float moveSpeed = 1.f;
+      float phi, theta;
+      glm::vec3 position;
+      glm::vec3 lookAt;
+
+      Camera();
+      Camera(glm::vec3);
+
+      void update(float, float);
+      void moveForward();
+      void moveBackward();
+      void moveLeft();
+      void moveRight();
+      void moveUp();
+      void moveDown();
+
+   private:
+      glm::vec3 u, v, w;
+};
+
+#endif
