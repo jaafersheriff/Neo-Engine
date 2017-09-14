c8f846d8974f455573ebcb5f01e32e596eb061f0
Jaafer Sheriff <jaafersheriff@gmail.com>
2017-09-13 20:35:54 -0700

    Finalize parent world

--- src/World/World.hpp
+++ src/World/World.hpp
@@ -1,3 +1,16 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//                           Abstract parent World class                               //
+//                                                                                     //
+// How to create a derived World:                                                      //
+//    World-specific parameters: data structure to be rendered                         //
+//    Constructor(): include name of world                                             //
+//    Init(): Create objects and initialize rendering data struct                      //
+//    prepareRenderer(): activate feature renderers and pass in proper data struct     //
+//    update(): Update features, call takeInput()                                      //
+//    takeInput(): update camera and features based on hardware input                  //
+//    cleanUp(): Clean up features                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 #ifndef _WORLD_HPP_
 #define _WORLD_HPP_
@@ -5,25 +18,27 @@
 #include "Renderer/MasterRenderer.hpp"
 #include "Context/Context.hpp"
 #include "Cameras/Camera.hpp"
+#include "Lights/Light.hpp"
+#include "Toolbox/Loader.hpp"
 
-#include <iostream>
-#include <string> // name
+#include <string>
+#include <vector>
 
 class World {
    public:
       World(const std::string n) : name(n) { }
       std::string name;
       Camera camera;
-      // TODO: std::vector<Light> lights;
+      std::vector<Light> lights;
 
       // Abstract functions
-      virtual void init(/* TODO: Loader loader*/) = 0;
-      virtual void prepareRenderer(const MasterRenderer &) = 0;
-      virtual void update(const Context &) = 0;
+      virtual void init(Loader *) = 0;
+      virtual void prepareRenderer(MasterRenderer &) = 0;
+      virtual void update(Context &) = 0;
       virtual void cleanUp() = 0;
 
    private:
-      virtual void takeInput(const Mouse &, const Keyboard &) = 0;
+      virtual void takeInput(Mouse &, Keyboard &) = 0;
 };
 
 #endif
