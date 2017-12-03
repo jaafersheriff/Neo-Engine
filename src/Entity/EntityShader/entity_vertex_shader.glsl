4ef36b4e2c9211e3a7291d9b56483e48020d0849
Jaafer Sheriff <jaafersheriff@gmail.com>
2017-12-03 00:38:02 -0800

    Revert to shading in fragment shader

--- src/Entity/EntityShader/entity_vertex_shader.glsl
+++ src/Entity/EntityShader/entity_vertex_shader.glsl
@@ -8,32 +8,16 @@ uniform mat4 P;
 uniform mat4 M;
 uniform mat4 V;
 
-uniform float matAmbient;
-uniform float shine;
-
-uniform vec3 lightPos;
-uniform vec3 lightCol;
-uniform vec3 lightAtt;
-
-out vec2 pass_textureCoords;
-out vec3 diffuseContrib;
-out vec3 specularContrib;
+out vec4 worldPos;
+out vec3 fragNormal;
+out vec4 viewDir;
+out vec2 textureCoords;
 
 void main() {
-    /* Per-vertex Blinn Phong */
-    vec4 worldPos = M * vertexPos;
-    vec3 lightDir = lightPos - worldPos.xyz;
-    vec3 L = normalize(lightDir);
-    vec3 Vi = normalize(vec3((inverse(V) * vec4(0.0, 0.0, 0.0, 1.0)) - worldPos));
-    vec3 N = normalize(vec3(M * vec4(vertexNormal, 0.0)));
-    vec3 H = (L + Vi) / 2;
-    
-    float lightDistance = length(lightDir);
-    float attFactor = lightAtt.x + lightAtt.y*lightDistance + lightAtt.z*lightDistance*lightDistance;
-
-    diffuseContrib = max(dot(L, N), matAmbient) * lightCol / attFactor;
-    specularContrib = pow(max(dot(H, N), 0), shine) * lightCol / attFactor;
-    pass_textureCoords = vertexTexture;
-
+    worldPos = M * vertexPos;
     gl_Position = P * V * worldPos;
+
+    viewDir = (inverse(V) * vec4(0.0, 0.0, 0.0, 1.0)) - worldPos;
+    fragNormal = vec3(M * vec4(vertexNormal, 0.0));
+    textureCoords = vertexTexture;
 }
