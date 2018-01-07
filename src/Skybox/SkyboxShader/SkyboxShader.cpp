c9d485b236356b347abda8f14fecbd50998a7534
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-01-07 14:48:12 -0800

    Collapse skybox renderer into shader

--- src/Skybox/SkyboxShader/SkyboxShader.cpp
+++ src/Skybox/SkyboxShader/SkyboxShader.cpp
@@ -1,10 +1,21 @@
 #include "SkyboxShader.hpp"
 
-bool SkyboxShader::init() {
+#include "glm/gtc/matrix_transform.hpp"
+
+bool SkyboxShader::init(Skybox *sb) {
     if (!Shader::init()) {
         return false;
     }
 
+    /* Set render target */
+    this->skybox = sb;
+
+    addAllLocations();
+
+    return true;
+}
+
+void SkyboxShader::addAllLocations() {
     /* Attributes */
     addAttribute("vertexPos");
 
@@ -14,8 +25,42 @@ bool SkyboxShader::init() {
 
     /* Cube texture */
     addUniform("cubeMap");
+}
 
-    return true;
+void SkyboxShader::setGlobals(const glm::mat4 *projection, const glm::mat4 *view) {
+    loadP(projection);
+
+    /* Center skybox areound camera */
+    glm::mat4 newView = glm::mat4(*view);
+    newView[3][0] = newView[3][1] = newView[3][2] = 0.f;
+    newView *= glm::rotate(glm::mat4(1.f), glm::radians(skybox->rotation), glm::vec3(0, 1, 0));
+    loadV(&newView);
+}
+
+void SkyboxShader::render(const World *world) {
+    /* Bind vertices */
+    glBindVertexArray(skybox->mesh->vaoId);
+    int pos = getAttribute("vertexPos");
+    glEnableVertexAttribArray(pos);
+    glBindBuffer(GL_ARRAY_BUFFER, skybox->mesh->vertBufId);
+    glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, (const void *) 0);
+
+    /* Bind texture */
+    loadCubeTexture(skybox->cubeTexture);
+    glActiveTexture(GL_TEXTURE0 + skybox->cubeTexture->textureId);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, skybox->cubeTexture->textureId);
+    
+    /* Draw */
+    glDrawArrays(GL_TRIANGLES, 0, skybox->mesh->vertBuf.size() / 3);
+
+    /* Unbind */
+    glDisableVertexAttribArray(getAttribute("vertexPos"));
+    Shader::unloadMesh();
+    Shader::unloadTexture(skybox->cubeTexture->textureId);
+}
+
+void SkyboxShader::cleanUp() {
+    Shader::cleanUp();
 }
 
 void SkyboxShader::loadP(const glm::mat4 *p) {
--- src/Skybox/SkyboxShader/SkyboxShader.hpp
+++ src/Skybox/SkyboxShader/SkyboxShader.hpp
@@ -1,17 +1,35 @@
+/* Skybox Shader class dervices Shader 
+ * Contains reference to skybox and entire rendering strategy */
 #pragma once
 #ifndef _SKYBOX_SHADER_HPP_
 #define _SKYBOX_SHADER_HPP_
 
 #include "Shader/Shader.hpp"
+#include "Skybox/Skybox.hpp"
 #include "Skybox/CubeTexture.hpp"
 
 class SkyboxShader : public Shader {
     public:
+        /* Define GLSL shader locations */
         SkyboxShader() : Shader("../src/Skybox/SkyboxShader/skybox_vertex_shader.glsl", 
                                 "../src/Skybox/SkyboxShader/skybox_fragment_shader.glsl") { }
         
-        bool init();
+        /* Reference to render target */
+        Skybox *skybox;
 
+        /* Init render target and local shaders */
+        bool init(Skybox *);
+
+        /* Render */
+        void render(const World *);
+
+        /* Wrap up and shut down */
+        void cleanUp();
+
+    private:
+        /* GLSL load functions */
+        void addAllLocations();
+        void setGlobals(const glm::mat4 *, const glm::mat4 *);
         void loadP(const glm::mat4 *);
         void loadV(const glm::mat4 *);
         void loadCubeTexture(const CubeTexture *);
