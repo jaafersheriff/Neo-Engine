04fb1b92f16cc6f5cc7f21696689e4d80bb4b071
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-01-07 14:38:56 -0800

    Collapse cloud renderer into shader

--- src/Cloud/CloudShader/CloudShader.cpp
+++ src/Cloud/CloudShader/CloudShader.cpp
@@ -1,10 +1,22 @@
 #include "CloudShader.hpp"
 
-bool CloudShader::init() {
+#include "glm/gtc/matrix_transform.hpp"
+
+bool CloudShader::init(std::vector<CloudBillboard *> *billboards) {
+    /* Parent init */
     if (!Shader::init()) {
         return false;
     }
 
+    /* Set render target */
+    this->billboards = billboards;
+
+    addAllLocations();
+
+    return true;
+}
+
+void CloudShader::addAllLocations() {
     /* Attributes */
     addAttribute("vertexPos");
 
@@ -23,8 +35,78 @@ bool CloudShader::init() {
     /* Light */
     addUniform("lightPos");
     addUniform("lightCol");
+}
 
-    return true;
+void CloudShader::setGlobals(const glm::mat4 *projection, const glm::mat4 *view) {
+    loadP(projection);
+    loadV(view);
+}
+
+/* Painters algorithm */
+void CloudShader::sortByDistance() {
+     for (int i = 0; i < billboards->size(); i++) {
+         int minSize = i; 
+         for (int j = i; j < billboards->size(); j++) {
+             if (billboards->at(j)->distance > billboards->at(minSize)->distance) {
+                 minSize = j;
+             }
+         }
+         if (minSize != i) {
+             CloudBillboard *tmp = billboards->at(i);
+             billboards->at(i) = billboards->at(minSize);
+             billboards->at(minSize) = tmp;
+         }
+     }
+}
+
+void CloudShader::render(const World *world) {
+    /* Skip rendering if no billboards exist */
+    if (!billboards->size()) {
+        return;
+    }
+
+    /* Forward render - single light source */
+    loadLight(world->light);
+
+    /* Handle stacked transparency */
+    glDisable(GL_DEPTH_TEST);
+
+    /* Iterate through every cloud billboards */
+    glm::mat4 M;
+    for (auto billboard : *billboards) {
+        /* Bind vertices */
+        // TODO : why do i need to do this per billboard?
+        glBindVertexArray(billboard->mesh->vaoId);
+        int pos = getAttribute("vertexPos");
+        glEnableVertexAttribArray(pos);
+        glBindBuffer(GL_ARRAY_BUFFER, billboard->mesh->vertBufId);
+        glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, (const void *)0);
+
+        /* Load billboard's position and size */
+        loadCenter(billboard->center);
+        loadSize(billboard->size);
+
+        /* Load billboard's rotation */
+        M = glm::rotate(glm::mat4(1.f), glm::radians(billboard->rotation), glm::vec3(0, 0, 1));
+        loadM(&M);
+
+        /* Load texture */
+        loadTexture(billboard->texture);
+        glActiveTexture(GL_TEXTURE0 + billboard->texture->textureId);
+        glBindTexture(GL_TEXTURE_2D, billboard->texture->textureId);
+
+        /* Draw */
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, billboard->mesh->vertBuf.size() / 3);
+
+        /* Unbind */
+        glDisableVertexAttribArray(getAttribute("vertexPos"));
+        Shader::unloadMesh();
+        Shader::unloadTexture(billboard->texture->textureId);
+    }
+}
+
+void CloudShader::cleanUp() {
+    Shader::cleanUp();
 }
 
 void CloudShader::loadP(const glm::mat4 *p) {
--- src/Cloud/CloudShader/CloudShader.hpp
+++ src/Cloud/CloudShader/CloudShader.hpp
@@ -1,26 +1,45 @@
+/* Cloud Shader class derives Shader
+ * Contains reference to list of cloud billboards and entire rendering strategy */
 #pragma once
 #ifndef _CLOUD_SHADER_HPP_
 #define _CLOUD_SHADER_HPP_
 
 #include "Shader/Shader.hpp"
+#include "Cloud/CloudBillboard.hpp"
 #include "Model/Texture.hpp"
 #include "Light/Light.hpp"
 
 class CloudShader : public Shader {
     public:
+        /* Define GLSL shader locations */
         CloudShader() : Shader("../src/Cloud/CloudShader/cloud_vertex_shader.glsl",
                                "../src/Cloud/CloudShader/cloud_fragment_shader.glsl") { }
-        bool init();
 
+        /* Reference to render tartget */
+        std::vector<CloudBillboard *> *billboards;
+
+        /* Init render target and local shaders */
+        bool init(std::vector<CloudBillboard *> *);
+
+        /* Render */
+        void render(const World *);
+
+        /* Wrap up and shut down */
+        void cleanUp();
+
+    private:
+        /* Utility functions */
+        void sortByDistance();    /* Sort billboards by distance to camera */
+
+        /* GLSL Load functions */
+        void addAllLocations();
+        void setGlobals(const glm::mat4 *, const glm::mat4 *);
         void loadP(const glm::mat4 *);
         void loadV(const glm::mat4 *);
         void loadM(const glm::mat4 *);
-
         void loadCenter(const glm::vec3);
         void loadSize(const glm::vec2);
-
         void loadTexture(const Texture *);
-
         void loadLight(const Light *);
 };
 
