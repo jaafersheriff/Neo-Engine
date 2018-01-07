7311515c733a21901de79990c2439cf81eeb9e26
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-01-07 10:44:47 -0800

    Collapse entity renderer into entity shader

--- src/Entity/EntityShader/EntityShader.cpp
+++ src/Entity/EntityShader/EntityShader.cpp
@@ -1,11 +1,22 @@
 #include "EntityShader.hpp"
 
-bool EntityShader::init() {
+#include "glm/gtc/matrix_transform.hpp"
+
+bool EntityShader::init(std::vector<Entity *> *ep) {
     /* Parent init */
     if (!Shader::init()) {
         return false;
     }
+    
+    /* Set render target */
+    this->entitiesPointer = ep;
 
+    addAllLocations();
+        
+    return true;
+}
+
+void EntityShader::addAllLocations() {
     /* Add attributes */
     addAttribute("vertexPos");
     addAttribute("vertexNormal");
@@ -31,8 +42,131 @@ bool EntityShader::init() {
     addUniform("lightPos");
     addUniform("lightCol");
     addUniform("lightAtt");
+}
 
-    return true;
+void EntityShader::setGlobals(const glm::mat4 *projection, const glm::mat4 *view) {
+    loadP(projection);
+    loadV(view);
+}
+
+void EntityShader::render(const World *world) {
+    /* Load world members */
+    // TODO : each world has a map of uniforms
+    /* Forward render -- there's only one light in the world */
+    loadLight(world->light);
+    loadCameraPos(world->camera->position);
+
+    /* Loop through every entity */
+    // TODO : batched render
+    glm::mat4 M;
+    for (auto &e : *entitiesPointer) {
+        /* If entity mesh doesn't contain geometry, skip it */
+        if (!e->mesh || !e->mesh->vertBuf.size()) {
+            continue;
+        }
+
+        /* Create and load model matrix */
+        M = glm::translate(glm::mat4(1.f), e->position);
+        M *= glm::rotate(glm::mat4(1.f), glm::radians(e->rotation.x), glm::vec3(1, 0, 0));
+        M *= glm::rotate(glm::mat4(1.f), glm::radians(e->rotation.y), glm::vec3(0, 1, 0));
+        M *= glm::rotate(glm::mat4(1.f), glm::radians(e->rotation.z), glm::vec3(0, 0, 1));
+        M *= glm::scale(glm::mat4(1.f), e->scale);
+        loadM(&M);
+
+        /* Load texture/material */
+        loadTexture(e->modelTexture);
+
+        /* Load mesh */
+        loadMesh(e->mesh);
+
+        /* Draw */
+        glDrawElements(GL_TRIANGLES, (int)e->mesh->eleBuf.size(), GL_UNSIGNED_INT, nullptr);
+
+        /* Unload mesh */
+        unloadMesh(e->mesh);
+
+        /* Unload texture/material */
+        unloadTexture(e->modelTexture);
+    }
+}
+
+/* Bind mesh attributes to VAO/VBOs 
+ * All Meshes are assumed to have valid vertices and element indices 
+ * Other attributes can be exlcluded */
+void EntityShader::loadMesh(const Mesh *mesh) {
+    /* Bind mesh VAO */
+    glBindVertexArray(mesh->vaoId);
+    
+    /* Bind vertex buffer VBO */
+    int pos = getAttribute("vertexPos");
+    glEnableVertexAttribArray(pos);
+    glBindBuffer(GL_ARRAY_BUFFER, mesh->vertBufId);
+    glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, (const void *)0);
+
+    /* Bind normal buffer VBO */
+    pos = getAttribute("vertexNormal");
+    if (pos != -1 && mesh->norBufId != 0) {
+        glEnableVertexAttribArray(pos);
+        glBindBuffer(GL_ARRAY_BUFFER, mesh->norBufId);
+        glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, (const void *)0);
+    }
+
+    /* Bind texture coordinate buffer VBO */
+    pos = getAttribute("vertexTexture");
+    if (pos != -1 && mesh->texBufId != 0) {
+        glEnableVertexAttribArray(pos);
+        glBindBuffer(GL_ARRAY_BUFFER, mesh->texBufId);
+        glVertexAttribPointer(pos, 2, GL_FLOAT, GL_FALSE, 0, (const void *)0);
+    }
+
+    /* Bind indices buffer VBO */
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh->eleBufId);
+}
+
+/* Bind material properties and  texture if provided */
+void EntityShader::loadTexture(const ModelTexture &modelTexture) {
+    /* Bind texture if it exists */
+    if(modelTexture.texture && modelTexture.texture->textureId != 0) {
+        loadUsesTexture(true);
+        loadTexture(modelTexture.texture);
+        glActiveTexture(GL_TEXTURE0 + modelTexture.texture->textureId);
+        glBindTexture(GL_TEXTURE_2D, modelTexture.texture->textureId);
+    }
+    else {
+        loadUsesTexture(false);
+    }
+
+    /* Bind materials */
+    loadMaterial(modelTexture.ambientColor, 
+                          modelTexture.diffuseColor, 
+                          modelTexture.specularColor);
+    loadShine(modelTexture.shineDamper);
+}
+
+/* Unbind mesh */
+void EntityShader::unloadMesh(const Mesh *mesh) {
+    glDisableVertexAttribArray(getAttribute("vertexPos"));
+
+    int pos = getAttribute("vertexNormal");
+    if (pos != -1) {
+        glDisableVertexAttribArray(pos);
+    }
+
+    pos = getAttribute("vertexTexture");
+    if (pos != -1) {
+        glDisableVertexAttribArray(pos);
+    }
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+/* Unbind model/texture*/
+void EntityShader::unloadTexture(const ModelTexture &modelTexture) {
+    if (modelTexture.texture) {
+        glActiveTexture(GL_TEXTURE0 + modelTexture.texture->textureId);
+    }
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void EntityShader::loadP(const glm::mat4 *p) {
--- src/Entity/EntityShader/EntityShader.hpp
+++ src/Entity/EntityShader/EntityShader.hpp
@@ -1,12 +1,15 @@
 /* Entity Shader class derives Shader
- * Maintains entity shader */
+ * Contains reference to a list of entities and entire rendering strategy */
 #pragma once
 #ifndef _ENTITY_SHADER_HPP_
 #define _ENTITY_SHADER_HPP_
 
+#include "Entity/Entity.hpp"
 #include "Shader/Shader.hpp"
 #include "Light/Light.hpp"
 #include "Model/Texture.hpp"
+
+#include <vector>
 
 class EntityShader : public Shader {
     public:
@@ -14,10 +17,30 @@ class EntityShader : public Shader {
         EntityShader() : Shader("../src/Entity/EntityShader/entity_vertex_shader.glsl",
                                 "../src/Entity/EntityShader/entity_fragment_shader.glsl") { }
         
-        /* Init local shaders */
-        bool init();
+        /* Reference to render target */
+        std::vector<Entity *> *entitiesPointer;
 
-        /* Load functions */
+        /* Init render target and local shaders */
+        bool init(std::vector<Entity *> *);
+
+        /* Render */
+        void render(const World *);
+
+        /* Wrap up and shut down */
+        void cleanUp();
+
+    private:
+        /* Mesh */
+        void loadMesh(const Mesh *);
+        void unloadMesh(const Mesh *);
+        
+        /* Texture */
+        void loadTexture(const ModelTexture &);
+        void unloadTexture(const ModelTexture &);
+
+        /* GLSL Load functions */
+        void addAllLocations();
+        void setGlobals(const glm::mat4 *, const glm::mat4 *);
         void loadP(const glm::mat4 *);
         void loadM(const glm::mat4 *);
         void loadV(const glm::mat4 *);
@@ -27,7 +50,6 @@ class EntityShader : public Shader {
         void loadLight(const Light *);
         void loadUsesTexture(const bool);
         void loadTexture(const Texture *);
-        void loadTime(const float);
 };
 
 #endif
