f33e6d64dd7b47747990a69265f560384a44fadc
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-01-10 22:30:53 -0800

    Store static model textures in game object

--- src/World/LabWorld/Block.cpp
+++ src/World/LabWorld/Block.cpp
@@ -1,7 +1,6 @@
 #include "Block.hpp"
-
-Block::Block(Mesh *m, ModelTexture mt, const glm::vec3 p, const glm::vec3 r, const glm::vec3 s, float vel) : 
-    Entity(m, mt, p, r, s) {
+Block::Block(Mesh *m, const glm::vec3 p, const glm::vec3 r, const glm::vec3 s, float vel) : 
+    Entity(m, alive, p, r, s) {
     this->velocity = vel;
     this->isHit = false;
     this->boundingBox = AABB(m);
@@ -12,9 +11,9 @@ void Block::update() {
         return;
     }
     // if (this->intersects(player)) { isHit = true; update modeltexture; return }
-    glm::vec3 newPos =  this->position = this->rotation * velocity;
+    //glm::vec3 newPos =  this->position = this->rotation * velocity;
     // if (newPos.x > terrain.scale || newPos.x < -terrain.scale || 
     //     newPos.z > terrain.scale || newPos.z < -terrain.scale) { 
     //  vel = -vel }
-    this->position = newPos;
+    //this->position = newPos;
 }
--- src/World/LabWorld/Block.hpp
+++ src/World/LabWorld/Block.hpp
@@ -7,11 +7,17 @@
 
 class Block : public Entity {
     public:
-        Block(Mesh *, ModelTexture, const glm::vec3, const glm::vec3, const glm::vec3, float);
+        Block(Mesh *, const glm::vec3, const glm::vec3, const glm::vec3, float);
         
         AABB boundingBox;
         bool isHit;
         float velocity;
+        
+        /* Define model textures for dead and alive game objects*/
+        ModelTexture alive = ModelTexture(0.3f, glm::vec3(1.f), glm::vec3(1.f, 1.f, 0.f));
+        ModelTexture dead = ModelTexture(0.3f, glm::vec3(1.f, 0.f, 0.f), glm::vec3(1.f, 1.f, 0.f));
+
+
 
         void update();
 };
