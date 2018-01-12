1288b1109c2dee8c30e764aab1ce964da70297b9
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-01-12 12:00:54 -0800

    Entity child update model matrix

--- src/World/LabWorld/Block.cpp
+++ src/World/LabWorld/Block.cpp
@@ -1,13 +1,15 @@
 #include "Block.hpp"
-Block::Block(Mesh *m, const glm::vec3 p, const glm::vec3 r, const glm::vec3 s, float vel) : 
-    Entity(m, alive, p, r, s) {
+Block::Block(Mesh *m, ModelTexture mt, const glm::vec3 p, const glm::vec3 r, const glm::vec3 s, float vel) : 
+    Entity(m, mt, p, r, s) {
     this->velocity = vel;
     this->isHit = false;
-    this->boundingBox = AABB(m, p);
+    this->boundingBox = AABB(m, this);
 }
 
 // TODO : collision w other blocks
 void Block::update(Entity *terrain, AABB player) {
+    Entity::update();
+    
     /* If already hit, skip */
     if (isHit) {
         return;
@@ -16,7 +18,7 @@ void Block::update(Entity *terrain, AABB player) {
     /* Test for player collsiion */
     if (this->boundingBox.intersect(player)) {
         isHit = true;
-        this->modelTexture = dead;
+        this->modelTexture.diffuse = glm::vec3(1.f, 0.f, 0.f);
         return;
     }
 
--- src/World/LabWorld/Block.hpp
+++ src/World/LabWorld/Block.hpp
@@ -7,15 +7,13 @@
 
 class Block : public Entity {
     public:
-        Block(Mesh *, const glm::vec3, const glm::vec3, const glm::vec3, float);
+        Block(Mesh *, ModelTexture, const glm::vec3, const glm::vec3, const glm::vec3, float);
         
         AABB boundingBox;
         bool isHit;
         float velocity;
         
         /* Define model textures for dead and alive game objects*/
-        ModelTexture alive = ModelTexture(0.3f, glm::vec3(1.f), glm::vec3(1.f, 1.f, 0.f));
-        ModelTexture dead = ModelTexture(0.3f, glm::vec3(1.f, 0.f, 0.f), glm::vec3(1.f, 1.f, 0.f));
 
         void update(Entity *, AABB);
 };
