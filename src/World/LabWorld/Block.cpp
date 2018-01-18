4f805e4758b132608685c19259510ee4680cfcdb
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-01-17 16:44:26 -0800

    Finalize game object movement

--- src/World/LabWorld/Block.cpp
+++ src/World/LabWorld/Block.cpp
@@ -1,9 +1,17 @@
 #include "Block.hpp"
 
-Block::Block(Mesh *m, ModelTexture mt, const glm::vec3 p, const glm::vec3 r, const glm::vec3 s, float vel) : 
-    Entity(m, mt, p, r, s) {
-    this->velocity = vel;
+#include "glm/gtc/matrix_transform.hpp"
+
+Block::Block(Mesh *m, ModelTexture mt, const glm::vec3 p, const float r, const glm::vec3 s, float vel) : 
+    Entity(m, mt, p, glm::vec3(0.f, r, 0.f), s) {
     this->isHit = false;
+    this->direction = glm::vec3(
+                            glm::rotate(
+                                glm::mat4(1.f), 
+                                glm::radians(r), 
+                                glm::vec3(0, 1, 0)) * 
+                            glm::vec4(0.f, 0.f, 1.f, 0.f));
+    this->velocity = vel;
     this->boundingSphere = BoundingSphere(m);
 }
 
@@ -23,12 +31,14 @@ void Block::update(Entity *terrain, BoundingSphere player) {
         return;
     }
 
-    // TODO : update direction/position
-    //glm::vec3 newPos =  this->position = this->rotation * velocity;
-    // if (newPos.x > terrain.scale || newPos.x < -terrain.scale || 
-    //     newPos.z > terrain.scale || newPos.z < -terrain.scale) { 
-    //  vel = -vel }
-    //this->position = newPos;
+
+    glm::vec3 newPos = this->position + this->direction * velocity;
+    this->position = newPos;
+
+    if (newPos.x > terrain->scale.x || newPos.x < -terrain->scale.z ||
+        newPos.z > terrain->scale.x || newPos.z < -terrain->scale.z) {
+        this->velocity = -this->velocity;
+    }
 
     // TODO : collision w other blocks
     // TODO : collision w terrain
--- src/World/LabWorld/Block.hpp
+++ src/World/LabWorld/Block.hpp
@@ -7,11 +7,12 @@
 
 class Block : public Entity {
     public:
-        Block(Mesh *, ModelTexture, const glm::vec3, const glm::vec3, const glm::vec3, float);
+        Block(Mesh *, ModelTexture, const glm::vec3, const float, const glm::vec3, float);
         
         BoundingSphere boundingSphere;
         bool isHit;
         float velocity;
+        glm::vec3 direction;
         
         /* Define model textures for dead and alive game objects*/
 
