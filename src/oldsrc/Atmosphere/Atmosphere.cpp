1fe411ac6fb2a716691fd82eebcfd460d7eeba5a
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-07-08 21:33:45 -0700

    complete clean up

--- src/oldsrc/Atmosphere/Atmosphere.cpp
+++
@@ -1,8 +0,0 @@
-#include "Atmosphere.hpp"
-
-Atmosphere::Atmosphere(Mesh *m, Texture *t1, Texture *t2, float size) {
-    this->mesh = m;
-    this->colorTexture = t1;
-    this->glowTexture = t2;
-    this->size = size;
-}
