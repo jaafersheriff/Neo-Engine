c443c1f8b737b07fe270d392c99046cb069ffaf0
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-07-09 19:50:55 -0700

    Introduce updating FPS

--- src/Engine/NeoEngine.cpp
+++ src/Engine/NeoEngine.cpp
@@ -1,15 +1,57 @@
 #include "NeoEngine.hpp"
 
+#include <time.h>
+#include <iostream>
+
 namespace neo {
 
+    std::string NeoEngine::RESOURCE_DIR = "../res/";
+    std::string NeoEngine::APP_NAME = "Neo Engine";
+
+    /* FPS */
+    int NeoEngine::FPS = 0;
+    int NeoEngine::nFrames = 0;
+    double NeoEngine::timeStep = 0.0;
+    double NeoEngine::lastFPSTime = 0.0;
+    double NeoEngine::lastFrameTime = 0.0;
+    double NeoEngine::runTime = 0.0;
+
     void NeoEngine::init(const std::string &title, const int width, const int height) {
-        Window::initGLFW(title);
+        srand((unsigned int)(time(0)));
+
+        APP_NAME = title;
+
+        /* Init window*/
+        if (Window::initGLFW(APP_NAME)) {
+            std::cerr << "Failed initializing Window" << std::endl;
+        }
         Window::setSize(glm::ivec2(width, height));
     }
 
     void NeoEngine::run() {
+
         while (!Window::shouldClose()) {
+            /* Update delta time and FPS */
+            updateFPS();
+
+            /* Update display, mouse, keyboard */
             Window::update();
+        }
+    }
+
+    void NeoEngine::shutDown() {
+        Window::shutDown();
+    }
+
+    void NeoEngine::updateFPS() {
+        runTime = glfwGetTime();
+        timeStep = runTime - lastFrameTime;
+        lastFrameTime = runTime;
+        nFrames++;
+        if (runTime - lastFPSTime >= 1.0) {
+            FPS = nFrames;
+            nFrames = 0;
+            lastFPSTime = runTime;
         }
     }
 }
--- src/Engine/NeoEngine.hpp
+++ src/Engine/NeoEngine.hpp
@@ -9,5 +9,20 @@ namespace neo {
         public:
             static void init(const std::string &, const int, const int);
             static void run();
+            static void shutDown();
+
+            static std::string RESOURCE_DIR;    /* Resource directory */
+            static std::string APP_NAME;        /* Name of application */
+            static int FPS;                     /* Frames per second */
+            static double timeStep;             /* Delta time */
+
+        private:
+            static void updateFPS();
+            static double lastFPSTime;      /* Time at which last FPS was calculated */
+            static int nFrames;             /* Number of frames in current second */
+            static double lastFrameTime;    /* Time at which last frame was rendered */
+            static double runTime;          /* Global timer */
+
+
     };
 }
