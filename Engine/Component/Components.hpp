d5c5f7caea07e9ae5dab06316f398e5f6b5c8fcb
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-08-02 01:02:59 -0700

    Light component

--- Engine/Component/Components.hpp
+++ Engine/Component/Components.hpp
@@ -1,6 +1,8 @@
 #include "CameraComponent/CameraControllerComponent.hpp"
 #include "CameraComponent/CameraComponent.hpp"
 
+#include "LightComponent/LightComponent.hpp"
+
 #include "SpatialComponent/SpatialComponent.hpp"
 
 #include "RenderableComponent/RenderableComponent.hpp"
---
+++ Engine/Component/LightComponent/LightComponent.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Component/Component.hpp"
+
+namespace neo {
+
+    class LightComponent : public Component {
+        public:
+            LightComponent(GameObject &go, const glm::vec3 &col = glm::vec3(1.f), const glm::vec3 att = glm::vec3(0.f)) :
+                Component(go),
+                color(col),
+                attenuation(att)
+            {}
+
+            /* Getters */
+            const glm::vec3 & getColor() const { return color; }
+            const glm::vec3 & getAttenuation() const { return attenuation; }
+
+            /* Setters */
+            void setColor(const glm::vec3 col) { this->color = col; }
+            void setAttenuation(const glm::vec3 att) { this->attenuation = att; }
+        private:
+            glm::vec3 color;
+            glm::vec3 attenuation;
+    };
+}
