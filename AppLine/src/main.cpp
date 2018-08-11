ec37f5bc22ccadb66a1b2b7faed44d7bf6b7ed33
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-08-11 00:25:11 -0700

    Orientation lines works

--- AppLine/src/main.cpp
+++ AppLine/src/main.cpp
@@ -3,6 +3,9 @@
 #include "CustomSystem.hpp"
 
 #include "Shader/LineShader.hpp"
+#include "Shader/WireframeShader.hpp"
+
+#include "glm/gtc/matrix_transform.hpp"
 
 using namespace neo;
 
@@ -71,20 +74,68 @@ struct Line {
     }
 };
 
+struct Orient {
+    GameObject *gameObject;
+    SpatialComponent *spatial;
+    RenderableComponent *renderable;
+    LineRenderable *uLine;
+    LineRenderable *vLine;
+    LineRenderable *wLine;
+
+    Orient(Mesh *mesh) {
+        gameObject = &NeoEngine::createGameObject();
+        spatial = &NeoEngine::addComponent<SpatialComponent>(*gameObject, glm::vec3(0.f), glm::vec3(1.f));
+        renderable = &NeoEngine::addComponent<RenderableComponent>(*gameObject, mesh);
+        renderable->addShaderType<WireframeShader>();
+        uLine = &NeoEngine::addComponent<LineRenderable>(*gameObject);
+        uLine->addShaderType<LineShader>();
+        uLine->lineColor = glm::vec3(1.f, 0.f, 0.f);
+        uLine->addNode(glm::vec3(0.f));
+        uLine->addNode(glm::vec3(1.f, 0.f, 0.f));
+        vLine = &NeoEngine::addComponent<LineRenderable>(*gameObject);
+        vLine->lineColor = glm::vec3(0.f, 1.f, 0.f);
+        vLine->addNode(glm::vec3(0.f));
+        vLine->addNode(glm::vec3(0.f, 1.f, 0.f));
+        vLine->addShaderType<LineShader>();
+        wLine = &NeoEngine::addComponent<LineRenderable>(*gameObject);
+        wLine->lineColor = glm::vec3(0.f, 0.f, 1.f);
+        wLine->addNode(glm::vec3(0.f));
+        wLine->addNode(glm::vec3(0.f, 0.f, 1.f));
+        wLine->addShaderType<LineShader>();
+
+        NeoEngine::addImGuiFunc("Orient", [&]() {
+            glm::vec3 pos = gameObject->getSpatial()->getPosition();
+            if (ImGui::SliderFloat3("Position", glm::value_ptr(pos), -10.f, 10.f)) {
+                gameObject->getSpatial()->setPosition(pos);
+            }
+            float scale = gameObject->getSpatial()->getScale().x;
+            if (ImGui::SliderFloat("Scale", &scale, 0.f, 10.f)) {
+                gameObject->getSpatial()->setScale(glm::vec3(scale));
+            }
+            static glm::vec3 rot(0.f);
+            if (ImGui::SliderFloat3("Rotation", glm::value_ptr(rot), 0.f, 4.f)) {
+                glm::mat4 R;
+                R = glm::rotate(glm::mat4(1.f), rot.x, glm::vec3(1, 0, 0));
+                R *= glm::rotate(glm::mat4(1.f), rot.y, glm::vec3(0, 1, 0));
+                R *= glm::rotate(glm::mat4(1.f), rot.z, glm::vec3(0, 0, 1));
+                gameObject->getSpatial()->setOrientation(glm::mat3(R));
+            }
+        });
+    }
+};
+
 int main() {
     NeoEngine::init("Diffuse Rendering", "res/", 1280, 720);
 
     /* Game objects */
     Camera camera(45.f, 0.01f, 100.f, glm::vec3(0, 0.6f, 5), 0.4f, 7.f);
-    Line line;
-    line.line->addNode(glm::vec3(0.f));
-    line.line->addNode(glm::vec3(0.f, 10.f, 0.f));
-    line.line->addNode(glm::vec3(10.f, 10.f, 0.f));
+    Orient(Loader::getMesh("cube"));
 
     /* Systems - order matters! */
     NeoEngine::addSystem<CustomSystem>();
     renderSystem = &NeoEngine::addSystem<RenderSystem>("shaders/");
     renderSystem->addShader<LineShader>();
+    renderSystem->addShader<WireframeShader>();
     NeoEngine::initSystems();
 
     /* Attach ImGui panes */
