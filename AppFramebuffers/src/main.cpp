9a8706bb815f26dc83c498f68703e360c29c6b14
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-08-16 22:49:47 -0700

    Introduce new project for testing frame buffer infrastructure

---
+++ AppFramebuffers/src/main.cpp
@@ -0,0 +1,109 @@
+#include <NeoEngine.hpp>
+
+#include "CustomSystem.hpp"
+
+#include "Shader/DiffuseShader.hpp"
+
+#include "glm/gtc/matrix_transform.hpp"
+
+using namespace neo;
+
+/* Systems */
+RenderSystem * renderSystem;
+
+/* Game object definitions */
+struct Camera {
+    Camera(float fov, float near, float far, glm::vec3 pos, float ls, float ms) {
+        GameObject *gameObject = &NeoEngine::createGameObject();
+        NeoEngine::addComponent<SpatialComponent>(gameObject, pos, glm::vec3(1.f));
+        NeoEngine::addComponent<CameraComponent>(gameObject, fov, near, far);
+        NeoEngine::addComponent<CameraControllerComponent>(gameObject, ls, ms);
+    }
+};
+
+struct Light {
+    GameObject *gameObject;
+    LightComponent *light;
+
+    Light(glm::vec3 pos, glm::vec3 col, glm::vec3 att) {
+        gameObject = &NeoEngine::createGameObject();
+        NeoEngine::addComponent<SpatialComponent>(gameObject, pos);
+        light = &NeoEngine::addComponent<LightComponent>(gameObject, col, att);
+
+        NeoEngine::addImGuiFunc("Light", [&]() {
+            glm::vec3 pos = gameObject->getSpatial()->getPosition();
+            if (ImGui::SliderFloat3("Position", glm::value_ptr(pos), -100.f, 100.f)) {
+                gameObject->getSpatial()->setPosition(pos);
+            }
+            glm::vec3 col = light->getColor();
+            if (ImGui::SliderFloat3("Color", glm::value_ptr(col), 0.f, 1.f)) {
+                light->setColor(col);
+            }
+            glm::vec3 att = light->getAttenuation();
+            ImGui::SliderFloat3("Attenuation", glm::value_ptr(att), 0.f, 1.f);
+            light->setAttenuation(att);
+        });
+    }
+};
+
+struct Orient {
+    GameObject *gameObject;
+    RenderableComponent *renderable;
+    Material material = Material(0.2f, glm::vec3(1,0,1));
+
+    Orient(Mesh *mesh) {
+        gameObject = &NeoEngine::createGameObject();
+        NeoEngine::addComponent<SpatialComponent>(gameObject, glm::vec3(0.f), glm::vec3(1.f));
+        renderable = &NeoEngine::addComponent<RenderableComponent>(gameObject, mesh);
+        renderable->addShaderType<DiffuseShader>();
+        NeoEngine::addComponent<MaterialComponent>(gameObject, &material);
+
+        NeoEngine::addImGuiFunc("Mesh", [&]() {
+            glm::vec3 pos = gameObject->getSpatial()->getPosition();
+            if (ImGui::SliderFloat3("Position", glm::value_ptr(pos), -10.f, 10.f)) {
+                gameObject->getSpatial()->setPosition(pos);
+            }
+            float scale = gameObject->getSpatial()->getScale().x;
+            if (ImGui::SliderFloat("Scale", &scale, 0.f, 10.f)) {
+                gameObject->getSpatial()->setScale(glm::vec3(scale));
+            }
+            static glm::vec3 rot(0.f);
+            if (ImGui::SliderFloat3("Rotation", glm::value_ptr(rot), 0.f, 4.f)) {
+                glm::mat4 R;
+                R = glm::rotate(glm::mat4(1.f), rot.x, glm::vec3(1, 0, 0));
+                R *= glm::rotate(glm::mat4(1.f), rot.y, glm::vec3(0, 1, 0));
+                R *= glm::rotate(glm::mat4(1.f), rot.z, glm::vec3(0, 0, 1));
+                gameObject->getSpatial()->setOrientation(glm::mat3(R));
+            }
+        });
+    }
+};
+
+int main() {
+    NeoEngine::init("Framebuffers", "res/", 1280, 720);
+
+    /* Game objects */
+    Camera camera(45.f, 1.f, 100.f, glm::vec3(0, 0.6f, 5), 0.4f, 7.f);
+    Light(glm::vec3(0.f, 2.f, 20.f), glm::vec3(1.f), glm::vec3(0.6, 0.2, 0.f));
+    Orient(Loader::getMesh("cube"));
+
+    /* Systems - order matters! */
+    NeoEngine::addSystem<CustomSystem>();
+    renderSystem = &NeoEngine::addSystem<RenderSystem>("shaders/");
+    renderSystem->addShader<DiffuseShader>();
+    NeoEngine::initSystems();
+
+    /* Attach ImGui panes */
+    NeoEngine::addImGuiFunc("Stats", [&]() {
+        ImGui::Text("FPS: %d", NeoEngine::FPS);
+        ImGui::Text("dt: %0.4f", NeoEngine::timeStep);
+        if (ImGui::Button("VSync")) {
+            Window::toggleVSync();
+        }
+    });
+
+    /* Run */
+    NeoEngine::run();
+
+    return 0;
+}
