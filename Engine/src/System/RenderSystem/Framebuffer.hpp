e633ae1d2b1220b26156001796ec71d5cb9e6705
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-08-17 19:34:27 -0700

    Enhance capabilities

--- Engine/src/System/RenderSystem/Framebuffer.hpp
+++ Engine/src/System/RenderSystem/Framebuffer.hpp
@@ -28,10 +28,21 @@ namespace neo {
 
             // TODO - ability to add multiple color attachments 
             // TODO - ability to add color render buffers
-            // TODO - if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
             void attachColorTexture(const Texture &texture) {
+                attachTexture(GL_COLOR_ATTACHMENT0, texture.textureId);
+            }
+
+            void attachDepthTexture(const Texture &texture) {
+                attachTexture(GL_DEPTH_ATTACHMENT, texture.textureId);
+            }
+
+        private:
+            void attachTexture(GLuint component, GLuint id) {
                 bind();
-                CHECK_GL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture.textureId, 0));
+                CHECK_GL(glFramebufferTexture2D(GL_FRAMEBUFFER, component, GL_TEXTURE_2D, id, 0));
+                if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+                    CHECK_GL(glCheckFramebufferStatus(GL_FRAMEBUFFER));
+                }
             }
     };
 }
