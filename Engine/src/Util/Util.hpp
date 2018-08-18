a00bbda7fbb727245ce534528b73030af65a09fa
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-08-17 20:48:06 -0700

    Introduce random functions

--- Engine/src/Util/Util.hpp
+++ Engine/src/Util/Util.hpp
@@ -9,6 +9,33 @@ namespace neo {
 
         static float PI() { return glm::pi<float>(); }
 
+
+        /* Generate a random float [0, 1] */
+        static inline float genRandom() {
+            return rand() / (float) RAND_MAX;
+        }
+    
+        /* Generate a scaled random value */
+        static inline float genRandom(const float val) {
+            return genRandom() * val;
+        }
+   
+        /* Generate a random value in a range [min, max] */
+        static inline float genRandom(const float min, const float max) {
+            return genRandom() * (max - min) + min;
+        }
+
+        /* Generate a random vec3 with values [0, 1] */
+        static inline glm::vec3 genRandomVec3() {
+            return glm::vec3(genRandom(), genRandom(), genRandom());
+        }
+    
+        /* Generate random vec3 with values [0, 1] */
+        static inline glm::vec3 genRandomVec3(const float min, const float max) {
+            return glm::vec3(genRandom(min, max), genRandom(min, max), genRandom(min, max));
+        }
+
+
         // rad is the sphere's radius
         // theta is CCW angle on xy plane
         // phi is angle from +z axis
