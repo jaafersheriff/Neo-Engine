7727b2c24b31e988b036b3247f84f3d8285df150
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-08-17 18:45:40 -0700

    Clean up

--- AppFramebuffers/src/main.cpp
+++ AppFramebuffers/src/main.cpp
@@ -134,7 +134,7 @@ int main() {
     Camera camera(45.f, 1.f, 100.f, glm::vec3(0, 0.6f, 5), 0.4f, 7.f);
     Light(glm::vec3(0.f, 2.f, 20.f), glm::vec3(1.f), glm::vec3(0.6, 0.2, 0.f));
     Renderable(Loader::getMesh("cube"));
-    Surveillance s(glm::vec3(-3, 0, 0), glm::vec3(1.f, 2.f, 1.f), glm::mat3(glm::rotate(glm::mat4(1.f), 1.4f, glm::vec3(0, 1, 0))));
+    Surveillance(glm::vec3(-3, 0, 0), glm::vec3(1.f, 2.f, 1.f), glm::mat3(glm::rotate(glm::mat4(1.f), 1.4f, glm::vec3(0, 1, 0))));
 
     /* Systems - order matters! */
     NeoEngine::addSystem<CustomSystem>();
