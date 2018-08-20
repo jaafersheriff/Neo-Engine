fe83efeff7516b28829bd2f5cfb569a1dfa6f8e1
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-08-19 17:57:15 -0700

    Fix delete

--- Engine/src/Model/Texture.hpp
+++ Engine/src/Model/Texture.hpp
@@ -10,17 +10,17 @@ namespace neo {
 
         public:
             virtual ~Texture() {
-                // TODO - delete textureId
+                CHECK_GL(glDeleteTextures(1, &textureId));
             }
 
             GLuint textureId = 0;
-
             int width, height, components;
 
             /* Upload to GPU */
             void upload(GLint, GLenum, GLint, GLenum, uint8_t * = nullptr);
             void uploadCubeMap(uint8_t **);
-    };
+            // TODO - virtual void upload(GLint, GLenum, GLint, GLenum, uint8_t ** = 0) = 0;
 
+    };
 
 }
