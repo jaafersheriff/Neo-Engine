68ebcfb3b79e36e065af9496e76d78dd3719ad28
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-09-02 20:47:27 -0700

    Better light set up

--- AppDeferred/src/main.cpp
+++ AppDeferred/src/main.cpp
@@ -45,25 +45,6 @@ struct Renderable {
         spat = &NeoEngine::addComponent<SpatialComponent>(gameObject, pos, scale);
         renderable = &NeoEngine::addComponent<RenderableComponent>(gameObject, mesh);
         NeoEngine::addComponent<MaterialComponent>(gameObject, material);
-
-        NeoEngine::addImGuiFunc("Mesh", [&]() {
-            glm::vec3 pos = gameObject->getSpatial()->getPosition();
-            if (ImGui::SliderFloat3("Position", glm::value_ptr(pos), -10.f, 10.f)) {
-                gameObject->getSpatial()->setPosition(pos);
-            }
-            float scale = gameObject->getSpatial()->getScale().x;
-            if (ImGui::SliderFloat("Scale", &scale, 0.f, 10.f)) {
-                gameObject->getSpatial()->setScale(glm::vec3(scale));
-            }
-            static glm::vec3 rot(0.f);
-            if (ImGui::SliderFloat3("Rotation", glm::value_ptr(rot), 0.f, 4.f)) {
-                glm::mat4 R;
-                R = glm::rotate(glm::mat4(1.f), rot.x, glm::vec3(1, 0, 0));
-                R *= glm::rotate(glm::mat4(1.f), rot.y, glm::vec3(0, 1, 0));
-                R *= glm::rotate(glm::mat4(1.f), rot.z, glm::vec3(0, 0, 1));
-                gameObject->getSpatial()->setOrientation(glm::mat3(R));
-            }
-        });
     }
 };
 
@@ -73,28 +54,30 @@ int main() {
     /* Game objects */
     Camera camera(45.f, 1.f, 100.f, glm::vec3(0, 0.6f, 5), 0.4f, 7.f);
     Light(glm::vec3(0.f, 2.f, 20.f), glm::vec3(1.f), glm::vec3(0.6, 0.2, 0.f));
-    for (int i = 0; i < 50; i++) {
+    for (int i = 0; i < 15; i++) {
         Light(
             glm::vec3(Util::genRandom(-45.f, 45.f), Util::genRandom(6.f, 15.f), Util::genRandom(-45.f, 45.f)),
             Util::genRandomVec3(0.f, 1.f),
-            glm::vec3(Util::genRandom(2.f, 5.f))
+            glm::vec3(Util::genRandom(25.f, 65.f))
         );
     }
 
     for (int i = 0; i < 50; i++) {
         Renderable r(
-            Util::genRandomBool() ? Loader::getMesh("cube") : Loader::getMesh("sphere"),
+            Loader::getMesh("cube"),
             glm::vec3(Util::genRandom(-45.f, 45.f), Util::genRandom(2.f, 5.f), Util::genRandom(-45.f, 45.f)),
             glm::vec3(Util::genRandom(5.f)));
         r.renderable->addShaderType<GBufferShader>();
-        r.material->ambient = 0.5f;
+        r.material->ambient = 0.7f;
         r.material->diffuse = Util::genRandomVec3();
+        r.material->specular = Util::genRandomVec3();
     }
 
     // Terrain 
     Renderable terrain(Loader::getMesh("quad"), glm::vec3(0.f, 0.f, 0.f), glm::vec3(100.f));
     terrain.spat->rotate(glm::mat3(glm::rotate(glm::mat4(1.f), -1.56f, glm::vec3(1, 0, 0))));
     terrain.material->diffuse = glm::vec3(0.7f);
+    terrain.material->ambient = 0.7f;
     terrain.renderable->addShaderType<GBufferShader>();
 
     /* Systems - order matters! */
