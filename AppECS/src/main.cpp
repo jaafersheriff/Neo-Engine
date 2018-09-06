f0b20a9fbea858e70d898e6db1e5bea87cc808e3
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-09-05 18:43:38 -0700

    Update imgui

--- AppECS/src/main.cpp
+++ AppECS/src/main.cpp
@@ -19,26 +19,6 @@ struct Camera {
         NeoEngine::addComponent<SpatialComponent>(gameObject, pos, glm::vec3(1.f));
         cameraComp = &NeoEngine::addComponent<CameraComponent>(gameObject, fov, near, far);
         cameraController = &NeoEngine::addComponent<CameraControllerComponent>(gameObject, ls, ms);
-
-        NeoEngine::addImGuiFunc("Camera", [&]() {
-            float fov = cameraComp->getFOV();
-            ImGui::SliderFloat("FOV", &fov, 0.f, 90.f);
-            cameraComp->setFOV(fov);
-
-            float near = cameraComp->getNear();
-            float far = cameraComp->getFar();
-            ImGui::SliderFloat("Near", &near, 0.f, 2.f);
-            ImGui::SliderFloat("Far", &far, 10.f, 10000.f);
-            cameraComp->setNearFar(near, far);
-
-            ImGui::SliderFloat("Look speed", &cameraController->lookSpeed, 0.f, 10.f);
-            ImGui::SliderFloat("Move speed", &cameraController->moveSpeed, 0.f, 10.f);
-
-            glm::vec3 position = gameObject->getSpatial()->getPosition();
-            ImGui::Text("Pos:     %0.2f, %0.2f, %0.2f", position.x, position.y, position.z);
-            glm::vec3 lookDir  = cameraComp->getLookDir();
-            ImGui::Text("lookDir: %0.2f, %0.2f, %0.2f", lookDir.x, lookDir.y, lookDir.z);
-        });
     }
 };
 
@@ -46,10 +26,14 @@ struct Renderable {
     GameObject *gameObject;
     RenderableComponent *renderComp;
     bool alive = false;
+    Mesh *mesh;
 
     Renderable(Mesh *m, glm::vec3 p, float s, glm::mat3 o = glm::mat3()) {
-        Mesh *mesh = m;
+        mesh = m;
+        init(p, s, o);
+   }
 
+    void init(glm::vec3 p, float s, glm::mat3 o) {
         gameObject = &NeoEngine::createGameObject();
         NeoEngine::addComponent<SpatialComponent>(gameObject, p, glm::vec3(s), o);
         renderComp = &NeoEngine::addComponent<RenderableComponent>(gameObject, mesh);
@@ -64,6 +48,9 @@ struct Renderable {
     void attachImGui(const std::string & name) {
         NeoEngine::addImGuiFunc(name, [&]() {
             if (!alive) {
+                if (ImGui::Button("Recreate")) {
+                    init(glm::vec3(0.f), 1.f, glm::mat3());
+                }
                 return;
             }
             if (ImGui::Button("Destroy")) {
@@ -128,30 +115,10 @@ int main() {
 
     /* Attach ImGui panes */
     cube.attachImGui("Cube");
-    NeoEngine::addImGuiFunc("Stats", [&]() {
-        ImGui::Text("FPS: %d", Util::FPS);
-        ImGui::Text("dt: %0.4f", Util::timeStep);
-        if (ImGui::Button("VSync")) {
-            Window::toggleVSync();
-        }
-    });
+    NeoEngine::addDefaultImGuiFunc();
     NeoEngine::addImGuiFunc("Mouse", [&]() {
         ImGui::Text("Mouse X, Y  : %0.2f, %0.2f", Mouse::x, Mouse::y);
         ImGui::Text("Mouse dx, dy: %0.2f, %0.2f", Mouse::dx, Mouse::dy);
-    });
-    NeoEngine::addImGuiFunc("Engine", [&]() {
-        ImGui::Text("GameObjects:  %d", NeoEngine::getGameObjects().size());
-        int count = 0;
-        for (auto go : NeoEngine::getGameObjects()) {
-            count += go->getAllComponents().size();
-        }
-        ImGui::Text("Components:  %d", count);
-        ImGui::Text("Systems: %d", NeoEngine::getSystems().size());
-    });
-    NeoEngine::addImGuiFunc("Systems", [&]() {
-        for (auto sys : NeoEngine::getSystems()) {
-            ImGui::Checkbox(sys.second->name.c_str(), &sys.second->active);
-        }
     });
 
     /* Run */
