0eaa5686fa82b946180ede32f3d6cef6603b41f4
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-09-05 18:37:18 -0700

    Improve imgui

--- AppDeferred/src/main.cpp
+++ AppDeferred/src/main.cpp
@@ -84,110 +84,100 @@ int main() {
     auto & lightPassShader = MasterRenderer::addSceneShader<LightPassShader>("lightpass.vert", "lightpass.frag");
 
     /* Attach ImGui panes */
-    NeoEngine::addImGuiFunc("Stats", [&]() {
-        ImGui::Text("FPS: %d", Util::FPS);
-        ImGui::Text("dt: %0.4f", Util::timeStep);
-        if (ImGui::Button("VSync")) {
-            Window::toggleVSync();
-        }
-    });
-    NeoEngine::addImGuiFunc("GBuffer", [&]() {
-        auto gbuffer = MasterRenderer::getFBO("gbuffer");
-        for (auto texture : gbuffer->textures) {
-            ImGui::Image((ImTextureID)texture->textureId, ImVec2(0.1f * texture->width, 0.1f * texture->height), ImVec2(0, 1), ImVec2(1, 0));
-        }
-    });
+    NeoEngine::addDefaultImGuiFunc();
 
-    NeoEngine::addImGuiFunc("Create Lights", [&]() {
-        ImGui::Checkbox("Show lights", &lightPassShader.showLights);
-        if (lightPassShader.showLights) {
-            ImGui::SameLine();
-            ImGui::SliderFloat("Show radius", &lightPassShader.showRadius, 0.f, 1.f);
-        }
-        static glm::vec3 pos(0.f);
-        static float size(15.f);
-        static glm::vec3 color(1.f);
-        static float yOffset(10.f);
-        ImGui::SliderFloat3("Position", glm::value_ptr(pos), -25.f, 25.f);
-        ImGui::SliderFloat("Scale", &size, 15.f, 100.f);
-        ImGui::SliderFloat3("Color", glm::value_ptr(color), 0.01f, 1.f);
-        ImGui::SliderFloat("Offset", &yOffset, 0.f, 25.f);
-        if (ImGui::Button("Create light")) {
-            auto light = new Light(pos, color, glm::vec3(size));
-            if (yOffset) {
-                NeoEngine::addComponent<SinMoveComponent>(light->gameObject, yOffset, pos.y);
+    NeoEngine::addImGuiFunc("Lights", [&]() {
+        static int index;
+        if (ImGui::CollapsingHeader("Create Lights")) {
+            if (ImGui::TreeNode("Single")) {
+                static glm::vec3 pos(0.f);
+                static float size(15.f);
+                static glm::vec3 color(1.f);
+                static float yOffset(10.f);
+                ImGui::SliderFloat3("Position", glm::value_ptr(pos), -25.f, 25.f);
+                ImGui::SliderFloat("Scale", &size, 15.f, 100.f);
+                ImGui::SliderFloat3("Color", glm::value_ptr(color), 0.01f, 1.f);
+                ImGui::SliderFloat("Offset", &yOffset, 0.f, 25.f);
+                if (ImGui::Button("Create")) {
+                    auto light = new Light(pos, color, glm::vec3(size));
+                    if (yOffset) {
+                        NeoEngine::addComponent<SinMoveComponent>(light->gameObject, yOffset, pos.y);
+                    }
+                    lights.push_back(light);
+                    index = lights.size() - 1;
+                }
+                ImGui::TreePop();
             }
-            lights.push_back(light);
+            if (ImGui::TreeNode("Random Lights")) {
+                if (ImGui::Button("Clear lights")) {
+                    for (auto & l : lights) {
+                        NeoEngine::removeGameObject(*l->gameObject);
+                    }
+                    lights.clear();
+                }
+                static int numLights = 10;
+                static glm::vec3 minOffset(0.f);
+                static glm::vec3 maxOffset(0.f);
+                static float minScale(0.f);
+                static float maxScale(10.f);
+                static float minSinOffset(0.f);
+                static float maxSinOffset(0.f);
+                ImGui::SliderInt("Num lights", &numLights, 0, 1000);
+                ImGui::SliderFloat3("Min offset", glm::value_ptr(minOffset), -100.f, 100.f);
+                ImGui::SliderFloat3("Max offset", glm::value_ptr(maxOffset), -100.f, 100.f);
+                ImGui::SliderFloat("Min scale", &minScale, 0.f, maxScale);
+                ImGui::SliderFloat("Max scale", &maxScale, minScale, 100.f);
+                ImGui::SliderFloat("Min sin", &minSinOffset, 0.f, 15.f);
+                ImGui::SliderFloat("Max sin", &maxSinOffset, 0.f, 15.f);
+                if (ImGui::Button("Create light")) {
+                    for (int i = 0; i < numLights; i++) {
+                        glm::vec3 position = glm::vec3(
+                            Util::genRandom(minOffset.x, maxOffset.x),
+                            Util::genRandom(minOffset.y, maxOffset.y),
+                            Util::genRandom(minOffset.z, maxOffset.z)
+                        );
+                        glm::vec3 color = Util::genRandomVec3();
+                        float size = Util::genRandom(minScale, maxScale);
+                        auto light = new Light(position, color, glm::vec3(size));
+                        NeoEngine::addComponent<SinMoveComponent>(light->gameObject, Util::genRandom(minSinOffset, maxSinOffset), position.y);
+                        lights.push_back(light);
+                    }
+                }
+                ImGui::TreePop();
+            }
         }
-    });
-    NeoEngine::addImGuiFunc("Edit Lights", [&]() {
-        static int index = 0;
-        if (!lights.size()) {
-            index = 0;
-            return;
-        }
-
-        ImGui::SliderInt("Index", &index, 0, lights.size()-1);
-        auto l = lights[index];
-        if (ImGui::Button("Delete light")) {
-            NeoEngine::removeGameObject(*l->gameObject);
-            lights.erase(lights.begin() + index);
-            index = glm::max(0, index - 1);
-            if (!lights.size()) {
+        if (ImGui::CollapsingHeader("Edit Lights")) {
+            ImGui::Checkbox("Show lights", &lightPassShader.showLights);
+            if (lightPassShader.showLights) {
+                ImGui::SameLine();
+                ImGui::SliderFloat("Show radius", &lightPassShader.showRadius, 0.01f, 1.f);
+            }
+            if (lights.empty()) {
                 return;
             }
-        }
-
-        auto spat = l->gameObject->getSpatial();
-        if (!spat) {
-            return;
-        }
-        glm::vec3 pos = spat->getPosition();
-        if (ImGui::SliderFloat3("Position", glm::value_ptr(pos), -25.f, 25.f)) {
-            spat->setPosition(pos);
-        }
-        float size = spat->getScale().x;
-        if (ImGui::SliderFloat("Scale", &size, 15.f, 100.f)) {
-            spat->setScale(glm::vec3(size));
-        }
-        glm::vec3 color = l->light->getColor();
-        if (ImGui::SliderFloat3("Color", glm::value_ptr(color), 0.01f, 1.f)) {
-            l->light->setColor(color);
-        }
-    });
-    NeoEngine::addImGuiFunc("Random lights", [&]() {
-        if (ImGui::Button("Clear lights")) {
-            for (auto & l : lights) {
+            ImGui::SliderInt("Index", &index, 0, lights.size() - 1);
+            auto l = lights[index];
+            if (ImGui::Button("Delete light")) {
                 NeoEngine::removeGameObject(*l->gameObject);
+                lights.erase(lights.begin() + index);
+                index = glm::max(0, index - 1);
             }
-            lights.clear();
-        }
-        static int numLights = 10;
-        static glm::vec3 minOffset(0.f);
-        static glm::vec3 maxOffset(0.f);
-        static float minScale(0.f);
-        static float maxScale(10.f);
-        static float minSinOffset(0.f);
-        static float maxSinOffset(0.f);
-        ImGui::SliderInt("Num lights", &numLights, 0, 1000);
-        ImGui::SliderFloat3("Min offset", glm::value_ptr(minOffset), -100.f, 100.f);
-        ImGui::SliderFloat3("Max offset", glm::value_ptr(maxOffset), -100.f, 100.f);
-        ImGui::SliderFloat("Min scale", &minScale, 0.f, maxScale);
-        ImGui::SliderFloat("Max scale", &maxScale, minScale, 100.f);
-        ImGui::SliderFloat("Min sin", &minSinOffset, 0.f, 15.f);
-        ImGui::SliderFloat("Max sin", &maxSinOffset, 0.f, 15.f);
-        if (ImGui::Button("Create light")) {
-            for (int i = 0; i < numLights; i++) {
-                glm::vec3 position = glm::vec3(
-                    Util::genRandom(minOffset.x, maxOffset.x),
-                    Util::genRandom(minOffset.y, maxOffset.y),
-                    Util::genRandom(minOffset.z, maxOffset.z)
-                );
-                glm::vec3 color = Util::genRandomVec3();
-                float size = Util::genRandom(minScale, maxScale);
-                auto light = new Light(position, color, glm::vec3(size));
-                NeoEngine::addComponent<SinMoveComponent>(light->gameObject, Util::genRandom(minSinOffset, maxSinOffset), position.y);
-                lights.push_back(light);
+            auto spat = l->gameObject->getSpatial();
+            if (!spat) {
+                return;
+            }
+            glm::vec3 pos = spat->getPosition();
+            if (ImGui::SliderFloat3("Position", glm::value_ptr(pos), -25.f, 25.f)) {
+                spat->setPosition(pos);
+            }
+            float size = spat->getScale().x;
+            if (ImGui::SliderFloat("Scale", &size, 15.f, 100.f)) {
+                spat->setScale(glm::vec3(size));
+            }
+            glm::vec3 color = l->light->getColor();
+            if (ImGui::SliderFloat3("Color", glm::value_ptr(color), 0.f, 1.f)) {
+                l->light->setColor(color);
+
             }
         }
     });
