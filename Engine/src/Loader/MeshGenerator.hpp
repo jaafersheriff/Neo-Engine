eee32b891b4afc1c33410f0ee38ec2b27524ca7e
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-09-07 16:58:59 -0700

    Replace old sphere generation with new icosphere

--- Engine/src/Loader/MeshGenerator.hpp
+++ Engine/src/Loader/MeshGenerator.hpp
@@ -129,109 +129,101 @@ namespace neo {
                      1.f, 1.f }, {}, GL_TRIANGLE_STRIP);
             }
 
-            // From https://stackoverflow.com/questions/5988686/creating-a-3d-sphere-in-opengl-using-visual-c/5989676#5989676
-            static Mesh * createSphere(float radius = 1.f, int rings = 30, int sectors = 30) {
-                std::vector<float> vertices, normals, texcoords;
-                std::vector<unsigned> indices;
-
-                float const R = 1.f/(float)(rings-1);
-                float const S = 1.f/(float)(sectors-1);
-
-                for(int r = 0; r < rings; ++r) {
-                    for(int s = 0; s < sectors; ++s) {
-                        texcoords.push_back(s*S);
-                        texcoords.push_back(r*R);
-
-                        glm::vec3 vert = glm::normalize(glm::vec3(
-                            glm::cos(2*Util::PI() * s * S) * glm::sin( Util::PI() * r * R ),
-                            glm::sin( -Util::PI()/2 + Util::PI() * r * R ),
-                            glm::sin(2*Util::PI() * s * S) * glm::sin( Util::PI() * r * R )));
-
-                        vertices.push_back(vert.x * radius);
-                        vertices.push_back(vert.y * radius);
-                        vertices.push_back(vert.z * radius);
-                        normals.push_back(vert.x);
-                        normals.push_back(vert.y);
-                        normals.push_back(vert.z);
-
-                        int curRow = r * sectors;
-                        int nextRow = (r+1) * sectors;
-
-                        indices.push_back(curRow + s);
-                        indices.push_back(nextRow + s);
-                        indices.push_back(nextRow + (s+1));
-
-                        indices.push_back(curRow + s);
-                        indices.push_back(nextRow + (s+1));
-                        indices.push_back(curRow + (s+1));
-                    }
-                }
-
-                return createMesh(vertices, normals, texcoords, indices);
- 
-            }
-
-            static Mesh * createIco(int recursions) {
+            // http://blog.andreaskahler.com/2009/06/creating-icosphere-mesh-in-code.html
+            static Mesh * createSphere(int recursions) {
 	            float t = (float) (1.f + (glm::sqrt(5.0)) / 2.f);
                 float length = glm::length(glm::vec3(1, 0, t));
                 std::vector<float> verts = {
-                    -1.f / length,    t / length,  0.f / length,
-                     1.f / length,    t / length,  0.f / length,
-                    -1.f / length,   -t / length,  0.f / length,
-                     1.f / length,   -t / length,  0.f / length,
-                     0.f / length, -1.f / length,    t / length,
-                     0.f / length,  1.f / length,    t / length,
-                     0.f / length, -1.f / length,   -t / length,
-                     0.f / length,  1.f / length,   -t / length,
-                       t / length,  0.f / length, -1.f / length,
-                       t / length,  0.f / length,  1.f / length,
-                      -t / length,  0.f / length, -1.f / length,
-                      -t / length,  0.f / length,  1.f / length
+                     -1.f / length,    t / length,  0.f / length,
+                      1.f / length,    t / length,  0.f / length,
+                     -1.f / length,   -t / length,  0.f / length,
+                      1.f / length,   -t / length,  0.f / length,
+                      0.f / length, -1.f / length,    t / length,
+                      0.f / length,  1.f / length,    t / length,
+                      0.f / length, -1.f / length,   -t / length,
+                      0.f / length,  1.f / length,   -t / length,
+                        t / length,  0.f / length, -1.f / length,
+                        t / length,  0.f / length,  1.f / length,
+                       -t / length,  0.f / length, -1.f / length,
+                       -t / length,  0.f / length,  1.f / length
                 };
-
 
                 std::vector<unsigned> ele = {
-                     0, 11,  5,
-                     0,  5,  1,
-                     0,  1,  7,
-                     0,  7, 10,
-                     0, 10, 11,
-                     1,  5,  9,
-                     5, 11,  4,
-                    11, 10,  2,
-                    10,  7,  6,
-                     7,  1,  8,
-                     3,  9,  4,
-                     3,  4,  2,
-                     3,  2,  6,
-                     3,  6,  8,
-                     3,  8,  9,
-                     4,  9,  5,
-                     2,  4, 11,
-                     6,  2, 10,
-                     8,  6,  7,
-                     9,  8,  1,
+                      0, 11,  5,
+                      0,  5,  1,
+                      0,  1,  7,
+                      0,  7, 10,
+                      0, 10, 11,
+                      1,  5,  9,
+                      5, 11,  4,
+                     11, 10,  2,
+                     10,  7,  6,
+                      7,  1,  8,
+                      3,  9,  4,
+                      3,  4,  2,
+                      3,  2,  6,
+                      3,  6,  8,
+                      3,  8,  9,
+                      4,  9,  5,
+                      2,  4, 11,
+                      6,  2, 10,
+                      8,  6,  7,
+                      9,  8,  1,
                 };
 
-                for (int i = 0; i < recursions; i++) {
-                    // TODO
+                for (int i = 0; i <= recursions; i++) {
+                    std::vector<unsigned> ele2 = ele;
+                    for (int j = 0; j < ele.size() - 3; j += 3) {
+                        // find 3 verts of old face
+                        glm::vec3 v1(verts[3 * ele[j + 0] + 0], verts[3 * ele[j + 0] + 1], verts[3 * ele[j + 0] + 2]);
+                        glm::vec3 v2(verts[3 * ele[j + 1] + 0], verts[3 * ele[j + 1] + 1], verts[3 * ele[j + 1] + 2]);
+                        glm::vec3 v3(verts[3 * ele[j + 2] + 0], verts[3 * ele[j + 2] + 1], verts[3 * ele[j + 2] + 2]);
+
+                        // add verts of new tris
+                        glm::vec3 halfA = glm::normalize((v1 + v2) / 2.f);
+                        glm::vec3 halfB = glm::normalize((v2 + v3) / 2.f);
+                        glm::vec3 halfC = glm::normalize((v3 + v1) / 2.f);
+                        verts.push_back(halfA.x);
+                        verts.push_back(halfA.y);
+                        verts.push_back(halfA.z);
+                        verts.push_back(halfB.x);
+                        verts.push_back(halfB.y);
+                        verts.push_back(halfB.z);
+                        verts.push_back(halfC.x);
+                        verts.push_back(halfC.y);
+                        verts.push_back(halfC.z);
+
+                        // add indices of new faces 
+                        int indA = verts.size() / 3 - 3;
+                        int indB = verts.size() / 3 - 2;
+                        int indC = verts.size() / 3 - 1;
+                        ele2.push_back(ele[j + 0]);
+                        ele2.push_back(indA);
+                        ele2.push_back(indC);
+                        ele2.push_back(ele[j + 1]);
+                        ele2.push_back(indB);
+                        ele2.push_back(indA);
+                        ele2.push_back(ele[j + 2]);
+                        ele2.push_back(indC);
+                        ele2.push_back(indB);
+                        ele2.push_back(indA);
+                        ele2.push_back(indB);
+                        ele2.push_back(indC);
+                    }
+
+                    ele = ele2;
                 }
 
-                std::vector<float> normals;
+                // calculate UV coords
                 std::vector<float> tex;
                 for (int i = 0; i < verts.size(); i += 3) {
-                    glm::vec3 n = glm::normalize(glm::vec3(verts[i], verts[i + 1], verts[i + 2]));
-                    normals.push_back(n.x);
-                    normals.push_back(n.y);
-                    normals.push_back(n.z);
-
-                    tex.push_back(glm::clamp(0.5f + std::atan2(n.z, n.x) / (2.f * Util::PI()), 0.f, 1.f));
-                    tex.push_back(glm::clamp(0.5f + std::asin(n.y) / Util::PI(), 0.f, 1.f));
+                    tex.push_back(glm::clamp(0.5f + std::atan2(verts[i + 2], verts[i]) / (2.f * Util::PI()), 0.f, 1.f));
+                    tex.push_back(glm::clamp(0.5f + std::asin(verts[i+1]) / Util::PI(), 0.f, 1.f));
                 }
 
                 return createMesh(
                     verts,
-                    normals,
+                    verts,
                     tex,
                     ele
                 );
