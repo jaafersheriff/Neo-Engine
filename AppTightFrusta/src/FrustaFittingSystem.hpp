bb63c7dc61d24f5178a16e061fef7cb457a2f3ad
Jaafer Sheriff <jaafersheriff@gmail.com>
2019-06-12 22:10:53 -0700

    Random movements for testing

--- AppTightFrusta/src/FrustaFittingSystem.hpp
+++ AppTightFrusta/src/FrustaFittingSystem.hpp
@@ -16,5 +16,19 @@ public:
     {}
 
     virtual void update(const float dt) override {
+        auto mockOrthoCamera = Engine::getSingleComponent<MockOrthoComponent>();
+        auto mockPerspectiveCamera = Engine::getSingleComponent<MockPerspectiveComponent>();
+        if (!mockOrthoCamera || !mockPerspectiveCamera) {
+            return;
+        }
+        auto orthoCamera = mockOrthoCamera->getGameObject().getComponentByType<CameraComponent>();
+        auto perspectiveCamera = mockPerspectiveCamera->getGameObject().getComponentByType<CameraComponent>();
+        if (!orthoCamera || !perspectiveCamera) {
+            return;
+        }
+        float f = glm::sin(Util::getRunTime());
+        float g = glm::cos(Util::getRunTime());
+        perspectiveCamera->setLookDir(glm::vec3(f, -0.2f, g));
+        orthoCamera->setLookDir(glm::vec3(f, -0.2f, g));
     }
 };
