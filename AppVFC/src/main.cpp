465e92c3853ec94c6008b388ad3491ac15e074f3
Jaafer Sheriff <jaafersheriff@gmail.com>
2019-08-31 18:30:29 -0700

    Create base project for view frustum culling

---
+++ AppVFC/src/main.cpp
@@ -0,0 +1,86 @@
+#include <Engine.hpp>
+
+#include "Shader/GammaCorrectShader.hpp"
+#include "Shader/PhongShader.hpp"
+#include "Shader/AlphaTestShader.hpp"
+
+#include "glm/gtc/matrix_transform.hpp"
+
+using namespace neo;
+
+/* Game object definitions */
+struct Camera {
+    CameraComponent *camera;
+    Camera(float fov, float near, float far, glm::vec3 pos, float ls, float ms) {
+        GameObject *gameObject = &Engine::createGameObject();
+        Engine::addComponent<SpatialComponent>(gameObject, pos, glm::vec3(1.f));
+        camera = &Engine::addComponentAs<PerspectiveCameraComponent, CameraComponent>(gameObject, near, far, fov);
+        Engine::addComponent<CameraControllerComponent>(gameObject, ls, ms);
+    }
+};
+
+struct Light {
+    Light(glm::vec3 pos, glm::vec3 col, glm::vec3 att) {
+        auto& gameObject = Engine::createGameObject();
+        Engine::addComponent<SpatialComponent>(&gameObject, pos);
+        Engine::addComponent<LightComponent>(&gameObject, col, att);
+
+        Engine::addImGuiFunc("Light", [&]() {
+            auto light = Engine::getSingleComponent<LightComponent>();
+            glm::vec3 pos = light->getGameObject().getSpatial()->getPosition();
+            if (ImGui::SliderFloat3("Position", glm::value_ptr(pos), -100.f, 100.f)) {
+                light->getGameObject().getSpatial()->setPosition(pos);
+            }
+            ImGui::SliderFloat3("Color", glm::value_ptr(light->mColor), 0.f, 1.f);
+            ImGui::SliderFloat3("Attenuation", glm::value_ptr(light->mAttenuation), 0.f, 1.f);
+        });
+    }
+};
+
+struct Renderable {
+    GameObject *gameObject;
+
+    Renderable(Mesh *mesh, glm::vec3 position = glm::vec3(0.f), glm::vec3 scale = glm::vec3(1.f), glm::vec3 rotation = glm::vec3(0.f)) {
+        gameObject = &Engine::createGameObject();
+        Engine::addComponent<MeshComponent>(gameObject, mesh);
+        Engine::addComponent<SpatialComponent>(gameObject, position, scale, rotation);
+    }
+};
+
+int main() {
+    Engine::init("Base", "res/", 1280, 720);
+
+    /* Game objects */
+    Camera camera(45.f, 1.f, 100.f, glm::vec3(0, 0.6f, 5), 0.4f, 7.f);
+    Light(glm::vec3(-100.f, 100.f, 100.f), glm::vec3(1.f), glm::vec3(0.f, 0.015f, 0.f));
+
+    /* Cube object */
+    for (int i = 0; i < 1000; i++) {
+        glm::vec3 position(Util::genRandom(-15.f, 15.f), 0.f, Util::genRandom(-15.f, 15.f));
+        Renderable cube(Library::getMesh("cube"), position, glm::vec3(Util::genRandom(0.2f, 1.f)));
+        Engine::addComponent<renderable::PhongRenderable>(cube.gameObject);
+        Engine::addComponent<MaterialComponent>(cube.gameObject, 0.2f, glm::normalize(position), glm::vec3(1.f));
+    }
+
+    /* Ground plane */
+    Renderable plane(Library::getMesh("quad"), glm::vec3(0.f), glm::vec3(30.f), glm::vec3(-Util::PI() / 2.f, 0.f, 0.f));
+    Engine::addComponent<renderable::AlphaTestRenderable>(plane.gameObject);
+    Engine::addComponent<DiffuseMapComponent>(plane.gameObject, Library::getTexture("grid.png"));
+
+    /* Systems - order matters! */
+    Engine::addSystem<CameraControllerSystem>();
+    Engine::initSystems();
+
+    /* Init renderer */
+    Renderer::init("shaders/", camera.camera);
+    Renderer::addSceneShader<PhongShader>();
+    Renderer::addSceneShader<AlphaTestShader>();
+    Renderer::addPostProcessShader<GammaCorrectShader>();
+
+    /* Attach ImGui panes */
+    Engine::addDefaultImGuiFunc();
+
+    /* Run */
+    Engine::run();
+    return 0;
+}
