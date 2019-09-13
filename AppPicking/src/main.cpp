565c5d60c0209b81e39de52f4d94dde90fd842e4
Jaafer Sheriff <jaafersheriff@gmail.com>
2019-09-12 21:41:39 -0700

    New project

---
+++ AppPicking/src/main.cpp
@@ -0,0 +1,100 @@
+#include <Engine.hpp>
+
+#include "Shader/PhongShader.hpp"
+#include "Shader/AlphaTestShader.hpp"
+
+#include "glm/gtc/matrix_transform.hpp"
+
+using namespace neo;
+
+/* Game object definitions */
+struct Camera {
+    CameraComponent *camera;
+    Camera(float fov, float near, float far, glm::vec3 pos, float ls, float ms) {
+        GameObject *gameObject = &Engine::createGameObject();
+        Engine::addComponent<SpatialComponent>(gameObject, pos, glm::vec3(1.f));
+        camera = &Engine::addComponentAs<PerspectiveCameraComponent, CameraComponent>(gameObject, near, far, fov);
+        Engine::addComponent<CameraControllerComponent>(gameObject, ls, ms);
+    }
+};
+
+struct Light {
+    Light(glm::vec3 pos, glm::vec3 col, glm::vec3 att) {
+        auto& gameObject = Engine::createGameObject();
+        Engine::addComponent<SpatialComponent>(&gameObject, pos);
+        Engine::addComponent<LightComponent>(&gameObject, col, att);
+
+        Engine::addImGuiFunc("Light", [&]() {
+            auto light = Engine::getSingleComponent<LightComponent>();
+            glm::vec3 pos = light->getGameObject().getSpatial()->getPosition();
+            if (ImGui::SliderFloat3("Position", glm::value_ptr(pos), -100.f, 100.f)) {
+                light->getGameObject().getSpatial()->setPosition(pos);
+            }
+            ImGui::SliderFloat3("Color", glm::value_ptr(light->mColor), 0.f, 1.f);
+            ImGui::SliderFloat3("Attenuation", glm::value_ptr(light->mAttenuation), 0.f, 1.f);
+        });
+    }
+};
+
+struct Renderable {
+    GameObject *gameObject;
+
+    Renderable(Mesh *mesh, glm::vec3 position = glm::vec3(0.f), glm::vec3 scale = glm::vec3(1.f), glm::vec3 rotation = glm::vec3(0.f)) {
+        gameObject = &Engine::createGameObject();
+        Engine::addComponent<MeshComponent>(gameObject, mesh);
+        Engine::addComponent<SpatialComponent>(gameObject, position, scale, rotation);
+
+        Engine::addImGuiFunc("Mesh", [&]() {
+            glm::vec3 pos = gameObject->getSpatial()->getPosition();
+            if (ImGui::SliderFloat3("Position", glm::value_ptr(pos), -10.f, 10.f)) {
+                gameObject->getSpatial()->setPosition(pos);
+            }
+            float scale = gameObject->getSpatial()->getScale().x;
+            if (ImGui::SliderFloat("Scale", &scale, 0.f, 10.f)) {
+                gameObject->getSpatial()->setScale(glm::vec3(scale));
+            }
+            static glm::vec3 rot(0.f);
+            if (ImGui::SliderFloat3("Rotation", glm::value_ptr(rot), 0.f, 4.f)) {
+                glm::mat4 R;
+                R = glm::rotate(glm::mat4(1.f), rot.x, glm::vec3(1, 0, 0));
+                R *= glm::rotate(glm::mat4(1.f), rot.y, glm::vec3(0, 1, 0));
+                R *= glm::rotate(glm::mat4(1.f), rot.z, glm::vec3(0, 0, 1));
+                gameObject->getSpatial()->setOrientation(glm::mat3(R));
+            }
+        });
+    }
+};
+
+int main() {
+    Engine::init("Picking", "res/", 1280, 720);
+
+    /* Game objects */
+    Camera camera(45.f, 1.f, 100.f, glm::vec3(0, 0.6f, 5), 0.4f, 7.f);
+    Light(glm::vec3(0.f, 2.f, 20.f), glm::vec3(1.f), glm::vec3(0.6, 0.2, 0.f));
+
+    /* Cube object */
+    Renderable cube(Library::getMesh("cube"), glm::vec3(0.f, 0.5f, 0.f));
+    Engine::addComponent<renderable::PhongRenderable>(cube.gameObject);
+    Engine::addComponent<MaterialComponent>(cube.gameObject, 0.2f, glm::vec3(1.f, 0.f, 1.f), glm::vec3(1.f));
+
+    /* Ground plane */
+    Renderable plane(Library::getMesh("quad"), glm::vec3(0.f), glm::vec3(15.f), glm::vec3(-Util::PI() / 2.f, 0.f, 0.f));
+    Engine::addComponent<renderable::AlphaTestRenderable>(plane.gameObject);
+    Engine::addComponent<DiffuseMapComponent>(plane.gameObject, Library::getTexture("grid.png"));
+
+    /* Systems - order matters! */
+    Engine::addSystem<CameraControllerSystem>();
+    Engine::initSystems();
+
+    /* Init renderer */
+    Renderer::init("shaders/", camera.camera);
+    Renderer::addSceneShader<PhongShader>();
+    Renderer::addSceneShader<AlphaTestShader>();
+
+    /* Attach ImGui panes */
+    Engine::addDefaultImGuiFunc();
+
+    /* Run */
+    Engine::run();
+    return 0;
+}
