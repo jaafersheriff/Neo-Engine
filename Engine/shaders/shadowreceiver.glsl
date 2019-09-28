856ec18f2895b20dac2e0c627624ff9e0a71b760
Jaafer Sheriff <jaafersheriff@gmail.com>
2019-09-28 16:21:21 -0700

    shadow map z catch

--- Engine/shaders/shadowreceiver.glsl
+++ Engine/shaders/shadowreceiver.glsl
@@ -1,21 +1,24 @@
 
 float getShadowVisibility(int pcfSize, sampler2D shadowMap, vec4 shadowCoord, float bias) {
+    if (shadowCoord.z < 0.0 || shadowCoord.z > 1.0) {
+        return 1.0;
+    }
 
-    float visibility = 1.f;
+    float visibility = 1.0;
     if (pcfSize > 0) {
-        float shadow = 0.f;
-        vec2 texelSize = 1.f / textureSize(shadowMap, 0);
+        float shadow = 0.0;
+        vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
         for (int x = -pcfSize; x <= pcfSize; x++) {
             for (int y = -pcfSize; y <= pcfSize; y++) {
                 float pcfDepth = texture(shadowMap, shadowCoord.xy + vec2(x, y) * texelSize).r;
-                shadow += shadowCoord.z - bias > pcfDepth ? 1.f : 0.f;
+                shadow += shadowCoord.z - bias > pcfDepth ? 1.0 : 0.0;
             }
         }
         shadow /= (2 * pcfSize + 1) * (2 * pcfSize + 1);
-        visibility = 1.f - shadow;
+        visibility = 1.0 - shadow;
     }
     else if (texture(shadowMap, shadowCoord.xy).r < shadowCoord.z - bias) {
-        visibility = 0.f;
+        visibility = 0.0;
     }
 
     return visibility;
