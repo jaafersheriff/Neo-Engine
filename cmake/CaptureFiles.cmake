59d6fac774d1262e6e6e9fc6e753b6a9f043a688
Jaafer Sheriff <jaafersheriff@gmail.com>
2022-01-28 20:06:54 -0800

    move files, submodules, cmake

---
+++ cmake/CaptureFiles.cmake
@@ -0,0 +1,26 @@
+function(groupSources SRCS TargetID) 
+	foreach(source ${SRCS}) 
+		# Get parent dir
+		get_filename_component(source_path "${source}" DIRECTORY)
+
+		string(REGEX REPLACE ".*/Neo/" "" source_path_base "${source_path}")
+		string(REGEX REPLACE "${TargetID}[/]" "" source_path_base "${source_path_base}")
+		string(REGEX REPLACE "[/]${TargetID}" "" source_path_base "${source_path_base}")
+
+		string(REPLACE "/" "\\" source_path_msvc "${source_path_base}")
+
+		source_group("${source_path_msvc}" FILES ${source})
+	endforeach()
+endfunction() 
+
+function(captureFiles CPP_FILES HPP_FILES)
+	GetTargetName(TargetID)
+	targetCaptureFiles("${CPP_FILES}" "${HPP_FILES}" "${TargetID}")
+endfunction()
+
+function(targetCaptureFiles CPP_FILES HPP_FILES TargetID)
+	groupSources("${CPP_FILES}" "${TargetID}")
+	groupSources("${HPP_FILES}" "${TargetID}")
+endfunction()
+
+
---
+++ cmake/InitLib.cmake
@@ -0,0 +1,40 @@
+function(GetTargetName TargetID)
+	get_filename_component(_TargetID ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+	set("${TargetID}" "${_TargetID}" PARENT_SCOPE)
+endfunction()
+
+function(InitLib) 
+	GetTargetName(TargetID)
+	InitLibWithName("${TargetID}")
+endfunction()
+
+function (InitLibWithName TargetID) 
+	message(STATUS "Generating Neo/${TargetID}")
+
+	project(${TargetID})
+	add_library(${TargetID})
+
+	include_directories(${CMAKE_CURRENT_SOURCE_DIR}/include)
+	
+	file(GLOB_RECURSE CPP_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/*.*)
+	file(GLOB_RECURSE HPP_FILES ${CMAKE_CURRENT_SOURCE_DIR}/include/*.*)
+	targetCaptureFiles("${CPP_FILES}" "${HPP_FILES}" "${TargetID}")
+
+	target_sources(${TargetID}
+	PRIVATE
+		${CPP_FILES}
+		${HPP_FILES}
+	)
+
+	target_include_directories(${TargetID}
+	PUBLIC
+		${CMAKE_CURRENT_SOURCE_DIR}/include
+	PRIVATE
+		"include/${TargetID}"
+	)
+
+	set_target_properties(${TargetID} PROPERTIES FOLDER "Neo")
+
+endfunction()
+
+
