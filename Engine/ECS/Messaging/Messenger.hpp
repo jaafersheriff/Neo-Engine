305b9b59463b5de67ef4805eb31915a3658a5e61
jaafersheriff <jaafersheriff@gmail.com>
2022-03-08 13:08:22 -0800

    something broke

--- Engine/ECS/Messaging/Messenger.hpp
+++ Engine/ECS/Messaging/Messenger.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "Message.hpp"
-#include "ECS/GameObject.hpp"
+#include "ECS/ECS.hpp"
 
 #include <vector>
 #include <unordered_map>
@@ -9,10 +9,9 @@
 #include <memory>
 #include <functional>
 
-namespace neo {
 
-    class ECS;
-    class GameObject;
+// TODO - probably use some internal entt thing
+namespace neo {
 
     class Messenger {
         public:
@@ -23,31 +22,31 @@ namespace neo {
 
             /* Sends out a message for any receivers of that message type to pick up
              * If gameObject is not null, first sends the message locally to receivers of only that object. */
-            template <typename MsgT, typename... Args> static void sendMessage(const GameObject* gameObject, Args &&... args);
+            template <typename MsgT, typename... Args> static void sendMessage(const ECS::Entity* gameObject, Args &&... args);
 
             /* Adds a receiver for a message type. If gameObject is null, the function will be called for all messages 
             of that type. If gameObject is not null, the function will be called for only messages of that type sent to that object */
-            template <typename MsgT> static void addReceiver(const GameObject * gameObject, const ReceiverFunc&);
+            template <typename MsgT> static void addReceiver(const ECS::Entity * gameObject, const ReceiverFunc&);
 
             static void relayMessages(ECS& ecs);
             static void clean();
 
         private:
-            static std::vector<std::tuple<const GameObject *, std::type_index, std::unique_ptr<Message>>> mMessages;
+            static std::vector<std::tuple<const ECS::Entity *, std::type_index, std::unique_ptr<Message>>> mMessages;
             static std::unordered_map<std::type_index, std::vector<ReceiverFunc>> mReceivers;
     };
 
     template <typename MsgT, typename... Args>
-    void Messenger::sendMessage(const GameObject *gameObject, Args &&... args) {
+    void Messenger::sendMessage(const ECS::Entity *gameObject, Args &&... args) {
         static_assert(std::is_base_of<Message, MsgT>::value, "MsgT must be a message type");
         mMessages.emplace_back(gameObject, typeid(MsgT), std::make_unique<MsgT>(std::forward<Args>(args)...));
     }
 
     template <typename MsgT>
-    void Messenger::addReceiver(const GameObject *gameObject, const ReceiverFunc& func) {
+    void Messenger::addReceiver(const ECS::Entity *gameObject, const ReceiverFunc& func) {
         static_assert(std::is_base_of<Message, MsgT>::value, "MsgT must be a message type");
 
-        auto& receiver = gameObject ? const_cast<GameObject *>(gameObject)->mReceivers : mReceivers;
+        auto& receiver = gameObject ? const_cast<ECS::Entity *>(gameObject)->mReceivers : mReceivers;
 		receiver[std::type_index(typeid(MsgT))].emplace_back(func);
 
     }
