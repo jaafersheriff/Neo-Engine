dbc0f0477fd0a7750bb427cf9569bf8229e2bd74
Jaafer Sheriff <jaafersheriff@gmail.com>
2022-03-09 13:33:24 -0800

    maybe figured out const ctuple

--- Engine/ECS/Component/CameraComponent/FrustumComponent.hpp
+++ Engine/ECS/Component/CameraComponent/FrustumComponent.hpp
@@ -27,14 +27,14 @@ namespace neo {
         glm::vec4 mFar{ 0.f, 0.f, 0.f, 0.f };
 
         // Test if an object is inside the frustum
-        bool isInFrustum(const SpatialComponent& spatial, const BoundingBoxComponent& box) {
+        bool isInFrustum(const SpatialComponent& spatial, const BoundingBoxComponent& box) const {
             glm::vec3 position = spatial.getPosition() + box.getCenter();
             glm::vec3 _scale = spatial.getOrientation() * spatial.getScale();
             float scale = std::max(_scale.x, std::max(_scale.y, _scale.z)) * box.getRadius();
             return isInFrustum(position, scale);
         }
 
-        bool isInFrustum(const glm::vec3 position, const float radius) {
+        bool isInFrustum(const glm::vec3 position, const float radius) const {
             return _distanceToPlane(mLeft, position) > -radius &&
                 _distanceToPlane(mRight, position) > -radius &&
                 _distanceToPlane(mBottom, position) > -radius &&
@@ -45,7 +45,7 @@ namespace neo {
 
     private:
 
-        float _distanceToPlane(glm::vec4 plane, glm::vec3 position) {
+        float _distanceToPlane(glm::vec4 plane, glm::vec3 position) const {
             return plane.x * position.x + plane.y * position.y + plane.z * position.z + plane.w;
         }
 
