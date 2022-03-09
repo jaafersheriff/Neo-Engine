d87d553b2f602b522657f8ea7525061277af394f
Jaafer Sheriff <jaafersheriff@gmail.com>
2022-03-08 23:13:28 -0800

    clean up

--- Engine/Renderer/GLObjects/GLHelper.hpp
+++ Engine/Renderer/GLObjects/GLHelper.hpp
@@ -27,17 +27,14 @@ namespace neo {
 
         namespace GLHelper {
 
-            // void printOpenGLErrors(char const * const Function, char const * const File, int const Line);
             void checkFrameBuffer();
             void printProgramInfoLog(GLuint program);
             void printShaderInfoLog(GLuint shader);
         }
 
     #ifdef DEBUG_MODE
-    // #define CHECK_GL(x) do { neo::GLHelper::printOpenGLErrors("{{BEFORE}} "#x, __FILE__, __LINE__); (x); neo::GLHelper::printOpenGLErrors(#x, __FILE__, __LINE__); } while (0)
     #define CHECK_GL_FRAMEBUFFER() do {GLHelper::checkFrameBuffer(); } while(0)
     #else
-    // #define CHECK_GL(x) (x)
     #define CHECK_GL_FRAMEBUFFER()
     #endif
 }
