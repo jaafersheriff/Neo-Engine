1007db699782685f003616c08cd3d5240117b288
Jaafer Sheriff <jaafersheriff@gmail.com>
2023-09-30 00:14:52 -0700

    Mid-2000s tech can leave >:o

--- Demos/DemoRegistration.hpp
+++ Demos/DemoRegistration.hpp
@@ -2,7 +2,6 @@
 #include "Compute/Compute.hpp"
 #include "FrustaFitting/FrustaFitting.hpp"
 // #include "Deferred/Deferred.hpp"
-// #include "GodRays/GodRays.hpp"
 // #include "Metaballs/Metaballs.hpp"
 // #include "NormalVisualizer/NormalVisualizer.hpp"
 // #include "PostProcess/PostProcess.hpp"
@@ -18,7 +17,6 @@ static std::vector<neo::IDemo*> sDemos = {
  	new FrustaFitting::Demo(),
  	new Compute::Demo(),
 // 	new Deferred::Demo(),
-// 	new GodRays::Demo(),
 // 	new Metaballs::Demo(),
 // 	new NormalVisualizer::Demo(),
 // 	new PostProcess::Demo(),
