d65255fd76d0dd2dea9e6298abfab6162aba884f
Jaafer Sheriff <jaafersheriff@gmail.com>
2023-11-07 12:55:04 -0800

    y u no

--- Engine/Renderer/GLObjects/ResolvedShaderInstance.cpp
+++ Engine/Renderer/GLObjects/ResolvedShaderInstance.cpp
@@ -276,12 +276,12 @@ namespace neo {
 
     void ResolvedShaderInstance::bindTexture(const char* name, const Texture& texture) const {
         ServiceLocator<Renderer>::ref().mStats.mNumSamplers++;
-        GLint bindingLoc = 0;
+        GLenum bindingLoc = GL_TEXTURE0;
         auto binding = mBindings.find(HashedString(name));
         if (binding != mBindings.end()) {
-            bindingLoc = binding->second;
+            bindingLoc += binding->second;
         }
-        glActiveTexture(GL_TEXTURE0 + bindingLoc);
+        glActiveTexture(bindingLoc);
         texture.bind();
         glUniform1i(_getUniform(name), texture.mTextureID);
 
