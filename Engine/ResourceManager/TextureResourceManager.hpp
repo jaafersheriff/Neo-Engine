9351aa08ebfac3913b888841b632e53f4ef443d3
Jaafer Sheriff <jaafersheriff@gmail.com>
2024-03-20 00:48:07 -0700

    Path for loading texture files

--- Engine/ResourceManager/TextureResourceManager.hpp
+++ Engine/ResourceManager/TextureResourceManager.hpp
@@ -27,12 +27,14 @@ namespace neo {
 		Texture& get(TextureHandle id);
 		const Texture& get(TextureHandle id) const;
 
+		[[nodiscard]] TextureHandle asyncLoad(const char* filePath, TextureFormat format) const;
 		[[nodiscard]] TextureHandle asyncLoad(HashedString id, TextureBuilder& textureDetails) const;
 
 		void clear();
 	private:
 		void _tick();
 		mutable std::vector<std::pair<TextureHandle, TextureBuilder>> mQueue;
+		mutable std::vector<std::pair<std::string, TextureFormat>> mFileLoadQueue;
 		using TextureCache = entt::resource_cache<Texture>;
 		TextureCache mTextureCache;
 		std::shared_ptr<Texture> mDummyTexture;
