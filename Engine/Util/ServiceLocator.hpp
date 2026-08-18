f8968ab1644ed024807a6a563e668cb86c161f5f
Jaafer Sheriff <jaafersheriff@gmail.com>
2026-08-18 14:37:56 -0700

    service locator wrapper

--- Engine/Util/ServiceLocator.hpp
+++ Engine/Util/ServiceLocator.hpp
@@ -1,6 +1,31 @@
+#pragma once
+
 #include <entt/locator/locator.hpp>
 
+#include <utility>
+
 namespace neo {
+
 	template<typename T>
-	using ServiceLocator = entt::service_locator<T>;
+	class ServiceLocator {
+	public:
+		ServiceLocator() = delete;
+
+		template<typename... Args>
+		static void set(Args &&... args) {
+			entt::locator<T>::emplace(std::forward<Args>(args)...);
+		}
+
+		[[nodiscard]] static T& ref() {
+			return entt::locator<T>::value();
+		}
+
+		[[nodiscard]] static bool empty() {
+			return !entt::locator<T>::has_value();
+		}
+
+		static void reset() {
+			entt::locator<T>::reset();
+		}
+	};
 }
