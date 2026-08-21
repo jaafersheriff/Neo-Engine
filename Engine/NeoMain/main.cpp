ceef12630cac0a7599f696517386ccc065385ac4
Jaafer Sheriff <jaafersheriff@gmail.com>
2026-08-20 21:06:55 -0700

    final - hook up enkiTS to neo

--- Engine/NeoMain/main.cpp
+++ Engine/NeoMain/main.cpp
@@ -2,11 +2,17 @@
 #include "DemoInfra/DemoWrangler.hpp"
 #include "DemoRegistration.hpp"
 
+// TODO - Temp scaffolding, remove
+#include <ext/enki_incl.hpp>
+#include "Util/Log/Log.hpp"
+
 #include <cstdlib>
 #include <vector>
 #include <memory>
 
 int main() {
+	NEO_LOG_I("enkiTS sees %u hardware threads", enki::GetNumHardwareThreads());
+
 	neo::Engine engine;
 	engine.init();
 	engine.run(std::move(neo::DemoWrangler(sCurrentDemo, sDemos)));
