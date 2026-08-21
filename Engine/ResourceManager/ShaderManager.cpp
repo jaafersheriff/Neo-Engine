bd19ed6b64b09df7d796751ef76fbb9a5a4c58f9
Jaafer Sheriff <jaafersheriff@gmail.com>
2026-08-20 20:23:04 -0700

    go crazy with stress transforms

--- Engine/ResourceManager/ShaderManager.cpp
+++ Engine/ResourceManager/ShaderManager.cpp
@@ -155,6 +155,7 @@ namespace neo {
 	}
 
 	void ShaderManager::imguiEditor() {
+		// Not thread safe.
 		mCache.forEach([](const CachedResource<SourceShader>& entry) {
 			auto& shader = entry.mResource;
 			if (ImGui::TreeNode(shader.mName.c_str())) {
